56116c29b6ef702bff96f8491da3ec942e3a0f89
nishant hegde <nhegde@netflix.com>
2019-05-06 17:06:34 -0700

    kata 0506

---
+++ 1/summing.py
@@ -0,0 +1,17 @@
+def sum_numbers(numbers=None):
+
+    """
+        Function that accepts a list and returns the sum of the numbers in the list.
+        If no argumet is provided it calculates the sum of 1 to 100.
+    """
+
+    s = 0
+
+    if numbers:
+        s = sum(numbers)
+    else:
+        for n in range(1,101)
+            s += n
+
+    return s
+
