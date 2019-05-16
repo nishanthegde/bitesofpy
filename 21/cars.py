53a89ef745858969bf3c99a421d29dfe89dc5713
nishant hegde <nhegde@netflix.com>
2019-05-16 10:19:12 -0700

    bite kata 0516

--- 21/cars.py
+++ 21/cars.py
@@ -9,11 +9,17 @@ cars = {
 
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models (original order)"""
-    pass
+
+    return ", ".join(j for j in cars['Jeep'])
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
+
+    first_models = [cars[k][0] for k in cars.keys()]
+
+    return first_models
+
     pass
 
 
@@ -27,3 +33,10 @@ def get_all_matching_models(cars=cars, grep='trail'):
 def sort_car_models(cars=cars):
     """sort the car models (values) and return the resulting cars dict"""
     pass
+
+jeeps = get_all_jeeps(cars)
+print(jeeps)
+print(type(jeeps))
+
+first = get_first_model_each_manufacturer(cars)
+print(first)
