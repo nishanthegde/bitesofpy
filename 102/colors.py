bac6c343c35ee8b3a45174d05286ec8da9bc92e5
nishant hegde <nhegde@netflix.com>
2019-05-25 08:36:17 -0700

    kata 0525

--- 102/colors.py
+++ 102/colors.py
@@ -3,9 +3,25 @@ VALID_COLORS = ['blue', 'yellow', 'red']
 
 def print_colors():
     """In the while loop ask the user to enter a color,
-       lowercase it and store it in a variable. Next check: 
-       - if 'quit' was entered for color, print 'bye' and break. 
+       lowercase it and store it in a variable. Next check:
+       - if 'quit' was entered for color, print 'bye' and break.
        - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
        - otherwise print the color in lower case."""
+
     while True:
-        pass
+        color = input('Enter a color or ''quit'' to exit: ')
+        color = color.lower()
+
+        if color == 'quit':
+            print('bye')
+        else:
+            if color not in [c.lower() for c in VALID_COLORS]:
+                print('Not a valid color')
+                continue
+            else:
+                print(f'{color}')
+        break
+
+# print(VALID_COLORS)
+# print_colors(Yellow)
+
