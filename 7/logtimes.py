02e2ee995c725f41930ae374560950ad6db210e2
nishant hegde <nhegde@netflix.com>
2019-06-03 13:20:08 -0700

    kata 0603

--- 7/logtimes.py
+++ 7/logtimes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import os
 import urllib.request
+import re
 
 SHUTDOWN_EVENT = 'Shutdown initiated'
 CURRENT_PATH = os.getcwd()
@@ -15,11 +16,6 @@ f = open(LOGFILE, 'r')
 loglines = f.readlines()
 f.close
 
-# with open(logfile) as f:
-#     loglines = f.readlines()
-
-# for you to code:
-
 def convert_to_datetime(line):
     """TODO 1:
        Extract timestamp from logline and convert it to a datetime object.
@@ -29,12 +25,21 @@ def convert_to_datetime(line):
        datetime(2014, 7, 3, 23, 27, 51)
     """
 
-    str = "shutdown"
+    # yr = datetime.strptime(line, '%Y')
+    # p = re.search("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", line).start()
+    # find timestamp in line. use find iter for cases with multiple occurrences
 
-    if str in line.lower():
-      ts_str = line[5:24]
-      print(ts_str)
+    iter = re.finditer("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}", line)
+    st_indices = [s.start(0) for s in iter]
 
+    # extract timestamp from string
+    # cases where there are multiple matches found, return first instance
+    ts_str = line[st_indices[0]:st_indices[0]+19]
+
+    # convert to string to datetime
+    ts_dt = datetime.strptime(ts_str, '%Y-%m-%dT%H:%M:%S')
+
+    return ts_dt
 
 
 def time_between_shutdowns(loglines):
@@ -43,7 +48,16 @@ def time_between_shutdowns(loglines):
        calculate the timedelta between the first and last one.
        Return this datetime.timedelta object.
     """
-    pass
+    # list of timestamps of shutdown initiations
+    shutdown_stamps = []
 
-for l in loglines:
-  convert_to_datetime(l)
+    # read in loglines
+    for l in loglines:
+      if "shutdown initiated" in l.lower():
+        ts_dt = convert_to_datetime(l)
+        shutdown_stamps.append(ts_dt)
+
+    time_delta = shutdown_stamps[-1] - shutdown_stamps[0]
+    return time_delta
+
+
