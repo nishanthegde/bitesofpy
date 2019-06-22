fa08673c1490bd98b773b7144644d72cd1b53beb
nishant hegde <nhegde@netflix.com>
2019-06-21 19:17:48 -0700

    kata submission 621

--- 6/pcc_stats.py
+++ 6/pcc_stats.py
@@ -2,12 +2,12 @@
    and what challenge is more popular by number of PRs"""
 from collections import Counter, namedtuple
 import os
-import urllib.request
+import urllib.request as ur
 
-# prep
-
-tempfile = os.path.join('/tmp', 'dirnames')
-urllib.request.urlretrieve('http://bit.ly/2ABUTjv', tempfile)
+# local = os.getcwd()
+local = '/tmp'
+tempfile = os.path.join(local, 'dirnames')
+ur.urlretrieve('http://bit.ly/2ABUTjv', tempfile)
 
 IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
 
@@ -15,8 +15,7 @@ users, popular_challenges = Counter(), Counter()
 
 Stats = namedtuple('Stats', 'user challenge')
 
-
-# code
+# dance
 
 def gen_files():
     """Return a generator of dir names reading in tempfile
@@ -32,8 +31,13 @@ def gen_files():
 
        -> use last column to filter out directories (= True)
     """
-    pass
+    with open(tempfile, 'r') as f:
+      for line in f:
+        line = line.strip()
+        if line.endswith('True'):
+          yield line
 
+    f.close()
 
 def diehard_pybites():
     """Return a Stats namedtuple (defined above) that contains the user that
@@ -42,4 +46,33 @@ def diehard_pybites():
        Calling this function on the dataset (held tempfile) should return:
        Stats(user='clamytoe', challenge=('01', 7))
     """
-    pass
+    prs = [] #empty list for all "True" lines in dirnames
+    lines = gen_files()
+    for l in lines:
+      prs.append(l)
+
+    #split to get challenge numebr
+    challenge = [pr.split('/',1)[0] for pr in prs]
+    #count challenges
+    popular_challenges = Counter(challenge)
+    #most popular challenge
+    most_pop_ch = popular_challenges.most_common(1)[0]
+
+    who = [pr.split('/',1)[1] for pr in prs]
+    #split again to get user
+    #count users
+    user = [w.split(',',1)[0] for w in who]
+    user = [u for u in user if u not in IGNORE]
+    users = Counter(user)
+
+    most_pop_user = users.most_common(1)[0][0]
+
+    res = Stats(user=most_pop_user, challenge=most_pop_ch)
+    return res
+
+
+
+
+
+
+
