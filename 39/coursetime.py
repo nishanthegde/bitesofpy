78c9859e6d6a4944b12ed16a4d52bc583d292b80
nishant hegde <nhegde@netflix.com>
2019-06-25 11:09:13 -0700

    kata submission 625a

--- 39/coursetime.py
+++ 39/coursetime.py
@@ -4,8 +4,8 @@ import re
 import urllib.request as ur
 
 # getting the data
-local = os.getcwd()
-# local = '/tmp'
+# local = os.getcwd()
+local = '/tmp'
 COURSE_TIMES = os.path.join(local, 'course_timings')
 # ur.urlretrieve('http://bit.ly/2Eb0iQF', COURSE_TIMES)
 
