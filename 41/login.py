144dcd3275acbf49895ba055fcb7103db0c6e8e7
nishant hegde <nhegde@netflix.com>
2019-06-27 10:32:16 -0700

    kata submission 627

--- 41/login.py
+++ 41/login.py
@@ -1,12 +1,30 @@
+from functools import wraps
+
 known_users = ['bob', 'julian', 'mike', 'carmen', 'sue']
 loggedin_users = ['mike', 'sue']
 
 
 def login_required(func):
-    pass
-
+    @wraps(func)
+    def wrapper(user):
+        if user not in known_users:
+            return f'please create an account'
+        else:
+            if user in loggedin_users:
+                return func(user)
+            else:
+                return f'please login'
+    return wrapper
 
 @login_required
 def welcome(user):
     '''Return a welcome message if logged in'''
-    pass
+    return f'welcome back {user}'
+
+# def main():
+#     msg = welcome('anonymous')
+#     print(welcome.__doc__ )
+
+
+# if __name__ == "__main__":
+#     main()
