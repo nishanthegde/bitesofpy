5a393a61cf20794f00c0ccfca0a75820b1ece6dc
nishant hegde <nhegde@netflix.com>
2019-07-01 13:26:45 -0700

    kata submission 0701a

--- 49/packt.py
+++ 49/packt.py
@@ -40,41 +40,40 @@ def get_book():
 
     return book
 
+# def main():
 
-def main():
+#     book = get_book()
+#     # print(type(book))
+#     print(book)
 
-    book = get_book()
-    # print(type(book))
-    print(book)
+#     # get text of first h2 tag. this is the title of the free e book
+#     # print(book.find_all('h2')[0].text.strip())
 
-    # get text of first h2 tag. this is the title of the free e book
-    # print(book.find_all('h2')[0].text.strip())
-
-    #free_book = book.find_all("div", class_="dotd-title")
-
-
-    # for bk in free_book:
-    #     temp = bk.find('h2')
-    #     if temp:
-    #         print(temp.text.strip())
+#     #free_book = book.find_all("div", class_="dotd-title")
 
 
-    # get title
-    # for div in book.findAll('div', attrs={'class':'dotd-title'}):
-    #     print(div.text.strip())
+#     # for bk in free_book:
+#     #     temp = bk.find('h2')
+#     #     if temp:
+#     #         print(temp.text.strip())
 
 
-    # get desc
-    # for outer_div in book.findAll('div', attrs={'class': 'dotd-main-book-summary float-left'}):
-    #     for inner_div in book.findAll('div', attrs={'class': None}):
-    #         print(inner_div.text)
-    # print(' '.join(book.find('div', {'class': 'dotd-main-book-summary float-left'}).stripped_strings))
+#     # get title
+#     # for div in book.findAll('div', attrs={'class':'dotd-title'}):
+#     #     print(div.text.strip())
 
-    # for s in book.find('div', {'class': 'dotd-main-book-summary float-left'}).stripped_strings:
-    #     print(s)
-    # write contents of the html file for checking
-    # with open("packt.html", "w") as file:
-    #     file.write(str(book))
 
-if __name__ == "__main__":
-    main()
+#     # get desc
+#     # for outer_div in book.findAll('div', attrs={'class': 'dotd-main-book-summary float-left'}):
+#     #     for inner_div in book.findAll('div', attrs={'class': None}):
+#     #         print(inner_div.text)
+#     # print(' '.join(book.find('div', {'class': 'dotd-main-book-summary float-left'}).stripped_strings))
+
+#     # for s in book.find('div', {'class': 'dotd-main-book-summary float-left'}).stripped_strings:
+#     #     print(s)
+#     # write contents of the html file for checking
+#     # with open("packt.html", "w") as file:
+#     #     file.write(str(book))
+
+# if __name__ == "__main__":
+#     main()
