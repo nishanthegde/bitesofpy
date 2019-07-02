5de54467be4558a5c342f19cef59bf91efe8dafe
nishant hegde <nhegde@netflix.com>
2019-07-02 09:15:51 -0700

    kata submission 0702

--- 12/validate.py
+++ 12/validate.py
@@ -11,5 +11,65 @@ USERS = (julian, bob, pybites)
 
 # define exception classes here
 
-def get_secret_token(username):
+class UserDoesNotExist(Exception):
+    """Raised when the user is not in USERS"""
     pass
+
+class UserAccessExpired(Exception):
+    """Raised when the user is expired"""
+    pass
+
+class UserNoPermission(Exception):
+    """Raised when the user is not admin"""
+    pass
+
+def get_secret_token(username):
+    """
+        Create a function that takes a username and checks for a valid user:
+
+        The username is in USERS,
+        the user is not expired, and
+        the user has the ADMIN role.
+        If those 3 conditions are met return SECRET.
+
+        If one of the conditions is not True raise an exception you define yourself: UserDoesNotExist, UserAccessExpired and UserNoPermission respectively. Check out the tests for more detail.
+
+        Have fun and keep calm and code in Python!
+    """
+    try:
+        secret = False #boolean flag to return SECRET
+
+        for n in USERS:
+            if n.name == username:
+                #user found in tuple of users
+                if n.expired == True:
+                    raise UserAccessExpired
+                else:
+                    #user not expired
+                    if n.role.lower() != 'admin':
+                        raise UserNoPermission
+                    else:
+                         #user is an admin
+                        secret = True
+
+        if not secret:
+            raise UserDoesNotExist
+        else:
+            return SECRET
+
+    except UserDoesNotExist:
+        print("User does not exist!")
+
+    except UserAccessExpired:
+        print("User access expired!")
+
+    except UserNoPermission:
+        print("User does not have permission!")
+
+
+# def main():
+
+    pass
+
+# if __name__ == "__main__":
+#     main()
