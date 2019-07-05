a8b5bd1bdd92df4dd8f67e5b1d62f201fd8deb73
nishant hegde <nhegde@netflix.com>
2019-07-05 12:02:32 -0700

    kata submission 0705

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,23 @@
-def friends_teams():
-    pass
+from itertools import permutations, combinations
+
+def friends_teams(friends, team_size=2, order_does_matter=False):
+    """
+        A function that takes a list of friends, a team_size (type int, default=2) and order_does_matter (type bool, default False),
+        and returns all possible teams.
+        If order matters (order_does_matter=True), the number of teams would be greater.
+    """
+    if order_does_matter:
+        res = permutations(friends, team_size)
+    else:
+        res = combinations(friends, team_size)
+
+    return (i for i in list(res))
+
+# def main():
+#     friends = 'Bob Dante Julian Martin'.split()
+#     combos = list(friends_teams(friends, team_size=3, order_does_matter=True))
+#     print(len(combos))
+#     print(combos)
+
+# if __name__ == "__main__":
+#     main()
