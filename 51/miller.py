197afacaecaa1b57e060403812a44f5598377a84
nishant hegde <nhegde@netflix.com>
2019-07-05 12:08:42 -0700

    kata submission

---
+++ 51/miller.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+BITE_CREATED_DT = datetime.strptime('2018-02-26 23:24:04', '%Y-%m-%d %H:%M:%S')
+
+
+def py2_earth_hours_left():
+    """Return how many hours, rounded to 2 decimals, Python 2 has
+       left on Planet Earth"""
+    pass
+
+
+def py2_miller_min_left():
+    """Return how many minutes, rounded to 2 decimals, Python 2 has
+       left on Planet Miller"""
+    pass
