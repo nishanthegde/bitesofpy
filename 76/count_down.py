be4c26bd87a37aa6c4a23c7cc6bb327c442650dd
nishant hegde <nhegde@netflix.com>
2019-07-12 12:42:35 -0700

    kata submission 0712

--- 76/count_down.py
+++ 76/count_down.py
@@ -1,7 +1,62 @@
 from functools import singledispatch
 
-
 @singledispatch
 def count_down(data_type):
-    # TODO: Learn how to use singledispatch!
-    pass
+    raise ValueError('Unsupported type')
+
+@count_down.register(float)
+@count_down.register(int)
+def _(data):
+    if isinstance(data, bool):
+        raise ValueError('Unsupported type')
+
+    data = str(data)
+    print('{}'.format(data))
+
+    for i in range(1, len(data), 1):
+        print('{}'.format(data[:-i]))
+
+@count_down.register(str)
+def _(data):
+    if data:
+        print('{}'.format(data))
+
+        for i in range(1, len(data), 1):
+            print('{}'.format(data[:-i]))
+
+@count_down.register(list)
+@count_down.register(set)
+@count_down.register(tuple)
+def _(data):
+    data = [str(e) for e in data]
+    print('{}'.format(''.join(data)))
+
+    for i in range(1, len(data), 1):
+        print('{}'.format(''.join(data[:-i])))
+
+@count_down.register(dict)
+def _(data):
+    data = [str(k) for k in data.keys()]
+    print('{}'.format(''.join(data)))
+
+    for i in range(1, len(data), 1):
+        print('{}'.format(''.join(data[:-i])))
+
+
+# def main():
+
+#     # count_down('nishant')
+#     # count_down(9)
+#     # count_down(['a','b'])
+#     # count_down('1234')
+#     # count_down([1, 2, 3, 4])
+#     # count_down(['1', '2', '3', '4'])
+#     # count_down((1, 2, 3, 4))
+#     # count_down(('1', '2', '3', '4'))
+#     # count_down({1, 2, 3, 4})
+#     # count_down({1: 'one', 2: 'two', 3:'three', 4:'four'} )
+#     # count_down({'1': 'one', '2': 'two', '3':'three', '4':'four'})
+#     count_down(12.34)
+
+# if __name__ == "__main__":
+#     main()
