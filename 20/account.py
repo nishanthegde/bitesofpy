81cc5f1b54f985250af261f383d55e74859d7809
nishant hegde <nhegde@netflix.com>
2019-07-19 14:33:07 -0700

    kata submission 719

--- 20/account.py
+++ 20/account.py
@@ -5,6 +5,10 @@ class Account:
 
     @property
     def balance(self):
+        if sum(self._transactions) < 0:
+            raise Exception("text").with_traceback(None)
+            return 0
+
         return sum(self._transactions)
 
     def __add__(self, amount):
@@ -13,5 +17,36 @@ class Account:
     def __sub__(self, amount):
         self._transactions.append(-amount)
 
-    # add 2 dunder methods here to turn this class 
+    # add 2 dunder methods here to turn this class
     # into a 'rollback' context manager
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        return True
+
+
+
+# def main():
+#     """
+#         The purpose of the context manager is to roll back any transaction that will make the balance go below 0 (debt != cool). The rest of the class is already defined so you can focus on the context manager part.
+#     """
+#     # print("write a context manager")
+
+#     account = Account()
+#     print(account.balance)
+#     # account + 10
+#     # account - 5
+#     # print(account._transactions)
+#     # print(account.balance)
+
+#     with account as acc:
+#         acc - 5
+
+#     print(account._transactions)
+#     print(account.balance)
+
+# if __name__ == "__main__":
+#     main()
+
