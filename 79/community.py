d822530799d98430cac78e7a46dd5609f1bbb903
nishant hegde <nhegde@netflix.com>
2019-07-21 16:52:49 -0700

    initiate kata

---
+++ 79/community.py
@@ -0,0 +1,17 @@
+import csv
+
+import requests
+
+CSV_URL = 'https://bit.ly/2HiD2i8'
+
+
+def get_csv():
+    """Use requests to download the csv and return the
+       decoded content"""
+    pass
+
+
+def create_user_bar_chart(content):
+    """Receives csv file (decoded) content and returns a table of timezones
+       and their corresponding member counts in pluses (see Bite/tests)"""
+    pass
