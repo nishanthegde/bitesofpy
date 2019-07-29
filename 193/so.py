56983be0d90882d983619a10519c009d6e5b0a28
nishant hegde <nhegde@netflix.com>
2019-07-29 10:34:02 -0700

    kata submission 0727a

--- 193/so.py
+++ 193/so.py
@@ -33,15 +33,36 @@ def top_python_questions(url=cached_so_url):
         for votes in question_summary.find_all("span", {"class": "vote-count-post"}):
             vote_list.append(votes.get_text().strip())
 
-    res = sorted([(ques_list[i],vote_list[i]) for i,v in enumerate(view_list) if 'm views' in v], key= lambda x:x[1], reverse=True)
+    res = sorted([(ques_list[i],int(vote_list[i])) for i,v in enumerate(view_list) if 'm views' in v], key= lambda x:x[1], reverse=True)
 
     return res
 
 # def main():
-#     actual_ret = top_python_questions()
-#     print(actual_ret)
-#     print(len(actual_ret))
-#     # print(len(actual_return))
+#     actual_return = top_python_questions()
+#     expected_return = [
+#                         ('What does the “yield” keyword do?', 9169),
+#                         ('Does Python have a ternary conditional operator?', 5135),
+#                         ('What does if __name__ == “__main__”: do?', 4927),
+#                         ('Calling an external command in Python', 4190),
+#                         ('How to merge two dictionaries in a single expression?', 3874),
+#                         ('How do I sort a dictionary by value?', 3394),
+#                         ('Using global variables in a function', 2768),
+#                         ('Understanding slice notation', 2707),
+#                         ('How to make a flat list out of list of lists', 2545),
+#                         ('How do I install pip on Windows?', 2388),
+#                         ('How do I pass a variable by reference?', 2295),
+#                         ('How to clone or copy a list?', 2063),
+#                         ('How to read a file line-by-line into a list?', 2000),
+#                         ('Converting string into datetime', 1816),
+#                         ('How to print without newline or space?', 1615),
+#                         ('Select rows from a DataFrame based on '
+#                          'values in a column in pandas', 1304),
+#                         ("Why does comparing strings using either '==' or 'is' "
+#                          'sometimes produce a different result?', 1008)
+#                         ]
+#     # print(actual_return)
+#     # print(expected_return)
+#     actual_return == expected_return
 
 # if __name__ == "__main__":
 #     main()
