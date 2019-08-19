8a0456819f147f9328b727a3708aef1a23e37856
nishant hegde <nhegde@netflix.com>
2019-08-19 09:57:28 -0700

    kata submission 819

--- 85/ninja.py
+++ 85/ninja.py
@@ -3,6 +3,11 @@ ranks = 'white yellow orange green blue brown black paneled red'.split()
 BELTS = dict(zip(scores, ranks))
 
 
+CONGRATS_MSG = ('Congrats, you earned {score} points '
+                'obtaining the PyBites Ninja {rank} Belt')
+NEW_SCORE_MSG = 'Set new score to {score}'
+
+
 class NinjaBelt:
 
     def __init__(self, score=0):
@@ -10,13 +15,61 @@ class NinjaBelt:
         self._last_earned_belt = None
 
     def _get_belt(self, new_score):
-        """Might be a useful helper"""
-        pass
+        # get all belt scores in BELTS that are lower
+        levels = [i for i in BELTS if i <= new_score]
+        if levels:
+            # get the highest belt
+            _new_belt = BELTS[max(levels)]
+            # check if new belt is different
+            if self._last_earned_belt != _new_belt:
+                self._last_earned_belt = _new_belt
+                return CONGRATS_MSG.format(score=self.score, rank=_new_belt.capitalize())
+
+        return NEW_SCORE_MSG.format(score=self.score)
 
     def _get_score(self):
-        pass
+        return self._score
 
     def _set_score(self, new_score):
-        pass
+        if not isinstance(new_score, int):
+            raise ValueError("score must be int")
+
+        if new_score < self.score:
+            raise ValueError("score must be higher than current score. Current score is {}".format(self.score))
+
+        self._score = new_score
+        print(self._get_belt(self.score))
 
     score = property(_get_score, _set_score)
+
+
+# def main():
+#     """
+#         Make sure it can only be assigned an integer,
+#         It can not be assigned a score lower than the current score,
+#         It checks in BELTS if a new belt was earned (for example going from 49 to 50 you earn the yellow belt), if so it stores it in self._last_earned_belt.
+#         For each new score print a message:
+#         If a new badge was earned: Congrats, you earned {score} points obtaining the PyBites Ninja {rank} Belt
+#         Else just the new score: Set new score to {score}
+#     """
+#     print("dance!")
+#     # print(BELTS)
+#     ninja = NinjaBelt()
+
+#     assert ninja.score == 0
+#     assert ninja._last_earned_belt is None
+
+#     ninja.score = 20
+#     ninja.score = 49
+
+#     # ninja.score = 'a'
+#     # ninja.score = 40
+
+#     ninja.score = 50
+#     ninja.score = 177
+
+#     assert ninja._last_earned_belt.lower() == 'green'
+
+
+# if __name__ == '__main__':
+#     main()
