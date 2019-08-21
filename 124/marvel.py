d65c49d15fc4c60a6ad10254d905c829abebd0cf
nishant hegde <nhegde@netflix.com>
2019-08-21 11:56:19 -0700

    kata submission 0821

--- 124/marvel.py
+++ 124/marvel.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
+from itertools import groupby
 import csv
 import re
+
+from collections import defaultdict
 
 import requests
 
@@ -8,8 +11,8 @@ MARVEL_CSV = 'https://raw.githubusercontent.com/pybites/marvel_challenge/master/
 
 Character = namedtuple('Character', 'pid name sid align sex appearances year')
 
-
 # csv parsing code provided so this Bite can focus on the parsing
+
 
 def _get_csv_data():
     """Download the marvel csv data and return its decoded content"""
@@ -35,13 +38,17 @@ def load_data():
 
 data = list(load_data())
 
-
 # start coding
+
 
 def most_popular_characters(top=5):
     """Get the most popular character by number of appearances,
        return top n characters (default 5)"""
-    pass
+    appeared = [x for x in data if x.appearances]
+    # print(len(appeared))
+    appeared = sorted(appeared, key=lambda x: int(x.appearances), reverse=True)
+
+    return [a.name for a in appeared][:top]
 
 
 def max_and_min_years_new_characters():
@@ -49,9 +56,46 @@ def max_and_min_years_new_characters():
        use either the 'FIRST APPEARANCE' or 'Year' column in the csv data, or
        the 'year' attribute of the namedtuple, return a tuple of
        (max_year, min_year)"""
-    pass
+    with_years = [(x.year, x.name) for x in data if x.year]
+
+    # for key, group in groupby(with_years, key=lambda x: x.year):
+    #     print(key, [j for j in group])
+
+    years = defaultdict(list)
+
+    for year, name in with_years:
+        years[year].append(name)
+
+    years = {k: len(v) for k, v in years.items()}
+    years_sorted = sorted(years.items(), key=lambda x: x[1], reverse=True)
+
+    return (years_sorted[:1][0][0], years_sorted[-1:][0][0])
 
 
 def percentage_female():
     """Get the percentage of female characters as percentage of all characters, rounded to 2 digits"""
-    pass
+    all_chars = len(data)
+    fem_chars = len([c for c in data if c.sex == 'Female Characters'])
+
+    return round((fem_chars / all_chars) * 100, 2)
+
+
+# def main():
+#     # print(data[:5])
+#     actual = most_popular_characters()
+#     # print(actual)
+#     expected = ['Spider-Man', 'Captain America', 'Wolverine', 'Iron Man', 'Thor']
+#     # print(expected)
+#     assert actual == expected
+
+#     ret = max_and_min_years_new_characters()
+#     # print(ret)
+#     expected = ('1993', '1958')
+#     # print(expected)
+#     assert ret == expected
+
+#     assert percentage_female() == 23.43
+
+
+# if __name__ == "__main__":
+#     main()
