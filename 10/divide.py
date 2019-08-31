95c557b899b2474e92a1421eb330f1ae4f665e6c
nishant hegde <nhegde@netflix.com>
2019-08-31 05:49:01 -0700

    kata submission 831

--- 10/divide.py
+++ 10/divide.py
@@ -1,2 +1,26 @@
 def positive_divide(numerator, denominator):
-    pass
+    try:
+        ret = numerator / denominator
+        if ret > 0:
+            return ret
+        else:
+            raise ValueError
+
+    except ZeroDivisionError:
+        print("Unexpected ZeroDivisionError!")
+        return 0
+
+
+# def main():
+#     print(positive_divide(1, 2))
+#     print(positive_divide(1, 0))
+#     print(positive_divide(-1, -2))
+#     print(positive_divide(1.5, 2))
+#     # print(positive_divide(1, 's'))
+#     # positive_divide([], 2)
+#     # print(positive_divide(1, -2))
+#     positive_divide(-1, 2)
+
+
+# if __name__ == '__main__':
+#     main()
