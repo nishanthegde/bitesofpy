cffb5e515cd6fcec8e53ba70a24872822e7a1ccb
nishant hegde <nhegde@netflix.com>
2019-09-02 11:14:38 +0530

    kata submission 0902 in

--- 215/license.py
+++ 215/license.py
@@ -9,7 +9,7 @@ def validate_license(key: str) -> bool:
     """Write a regex that matches a PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
     """
-    key_reg = re.compile(r'^PB(-[0-9A-Z]{8}){4}')
+    key_reg = re.compile(r'^PB(-[0-9A-Z]{8}){4}$')
 
     if key_reg.search(key):
         return True
@@ -28,9 +28,25 @@ def validate_license(key: str) -> bool:
 
 #     for _ in range(10):
 #         key = _create_license()
-#         print(validate_license(key))
+#         # print(validate_license(key))
+#         assert validate_license(key)
 
-#     print(validate_license('test this out'))
+#     pool = [_create_license() for _ in range(5)]
+#     # print(pool)
+#     lcase_key = pool[0].lower()
+#     assert not validate_license(lcase_key)
+#     # print(validate_license('test this out'))
+#     shorter_key = pool[1][:-2]
+#     assert not validate_license(shorter_key)
+#     longer_key = pool[2] + 'A'
+#     assert not validate_license(longer_key)
+#     # print(longer_key)
+#     wrong_prefix = 'AB-' + pool[3][3:]
+#     assert not validate_license(wrong_prefix)
+#     empty_key = ''
+#     assert not validate_license(empty_key)
+#     key_reversed = pool[4][::-1]
+#     assert not validate_license(key_reversed)
 
 
 # if __name__ == '__main__':
