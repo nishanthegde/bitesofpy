84eeecfd7ab813373e76a353a67b8f2804c7dd12
nishant hegde <nhegde@netflix.com>
2019-09-10 04:58:34 -0700

    kata submission 0910

--- 197/mday.py
+++ 197/mday.py
@@ -4,4 +4,21 @@ from datetime import date
 def get_mothers_day_date(year):
     """Given the passed in year int, return the date Mother's Day
        is celebrated assuming it's the 2nd Sunday of May."""
-    pass
+    # loop over every day in May of the year
+
+    count = 0
+
+    for i in range(1, 32):
+        d = date(year, 5, i)
+        if d.weekday() == 6:
+            count += 1
+        if count == 2:
+            return d
+
+
+# def main():
+#     print(get_mothers_day_date(2021))
+
+
+# if __name__ == '__main__':
+#     main()
