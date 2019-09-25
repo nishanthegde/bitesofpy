bc68f391ce2c9a72119fda5b926a9a9e90c9d340
nishant hegde <nhegde@netflix.com>
2019-09-25 11:15:01 -0700

    kata submission 0925

--- 125/tribe.py
+++ 125/tribe.py
@@ -3,6 +3,7 @@ from collections import Counter
 from bs4 import BeautifulSoup
 import requests
 
+
 AMAZON = "amazon.com"
 TIM_BLOG = 'https://bit.ly/2NBnZ6P'
 
@@ -19,4 +20,34 @@ def get_top_books(content=None, limit=5):
        and count them, return the top "limit" books (default 5)"""
     if content is None:
         content = load_page()
-    # code here ...
+
+    books = []
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    for s in soup.find_all("div", {"class": "entry-content"}):
+        for element in s.find_all("a", href=lambda href: href and AMAZON in href):
+            books.append(element.get_text().strip())
+
+    c = Counter(books)
+
+    return [b[0] for b in c.most_common(limit)]
+
+
+# def main():
+
+#     print('here ...')
+#     content = load_page()
+#     books = get_top_books(content=content)
+
+#     assert books[0] == 'Man’s Search For Meaning'
+#     assert books[1] == 'Tao Te Ching'
+
+#     assert sorted(books[2:5]) == ['Sapiens: A Brief History of Humankind',
+#                                   ('The 4-Hour Workweek: Escape the 9-5, Live '
+#                                    'Anywhere and Join the New Rich'),
+#                                   'The Fountainhead']
+
+
+# if __name__ == '__main__':
+#     main()
