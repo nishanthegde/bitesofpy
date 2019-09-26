61dbc195b0e21f79b59b05d64cabb7c8362ca52a
nishant hegde <nhegde@netflix.com>
2019-09-26 05:51:39 -0700

    kata submission 0926

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,30 @@
+import re
+from collections import Counter
+
+
 def get_index_different_char(chars):
-    pass
+
+    alnum_list = [bool(re.match('^[a-zA-Z0-9]+$', str(e))) for e in chars]
+
+    c = Counter(alnum_list)
+
+    minority = c.most_common()[-1:][0][0]
+
+    return alnum_list.index(minority)
+
+
+# def main():
+
+#     print('here ...')
+#     ret = get_index_different_char(['A', 'f', '.', 'Q', 2])
+#     print(ret)
+
+#     ret = get_index_different_char(['.', '{', ' ^', '%', 'a'])
+#     print(ret)
+
+#     ret = get_index_different_char(list(range(1, 9)) + ['}'] + list('abcde'))
+#     print(ret)
+
+
+# if __name__ == '__main__':
+#     main()
