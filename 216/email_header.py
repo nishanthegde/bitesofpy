8a6cb27c40742d7cfeba65b9499c1cce63e64928
nishant hegde <nhegde@netflix.com>
2019-09-28 12:25:30 -0700

    kata submission 0928

--- 216/email_header.py
+++ 216/email_header.py
@@ -1,4 +1,5 @@
 import re
+import inspect
 
 EMAIL_HEADER = """Return-Path: <bounces+5555-7602-redacted-info>
 ...
@@ -18,6 +19,17 @@ Date: Wed, 19 Jun 2013 17:09:33 +0000 (UTC)
 X-SG-EID: P3IPuU2e1Ijn5xEegYUQ...
 X-SendGrid-Contentd-ID: {"test_id":"1371661776"}"""  # noqa E501
 
+OTHER_HEADER = """
+Return-Path: <info@pybit.es>
+From: Bob & Julian from PyBites (info@pybit.es)
+To: pybites@ninja.com
+Subject: New regex learning path!
+Date: Sun, 18 Aug 2019 17:16:10 -0700 (PDT)
+Envelope-To: pybites@ninja.com
+...
+X-SendGrid-Contentd-ID: {"test_id":"1371661776"}
+"""
+
 
 def get_email_details(header: str) -> dict:
     """User re.search or re.match to capture the from, to, subject
@@ -25,4 +37,37 @@ def get_email_details(header: str) -> dict:
        https://docs.python.org/3.7/library/re.html#re.Match.groupdict
        If not match, return None
     """
-    pass
+
+    match = re.search(r'^From:\s*(?P<from>.*)$[\s\S]*^To:\s*(?P<to>.*)$[\s\S]*^Subject:\s*(?P<subject>.*)$[\s\S]*^Date:\s*(?P<date>[^+-]+)\s', header, re.MULTILINE)
+
+    if match:
+        return match.groupdict()
+
+
+# def main():
+
+#     print('here ...')
+
+#     src = inspect.getsource(get_email_details)
+#     assert 're.match' in src or 're.search' in src
+#     assert 'groupdict' in src
+
+#     actual = get_email_details(EMAIL_HEADER)
+#     expected = {'from': 'redacted-address',
+#                 'to': 'redacted-address',
+#                 'subject': 'A Test From SendGrid',
+#                 'date': 'Wed, 19 Jun 2013 17:09:33'}
+#     assert actual == expected
+
+#     actual = get_email_details(OTHER_HEADER)
+#     expected = {'from': 'Bob & Julian from PyBites (info@pybit.es)',
+#                 'to': 'pybites@ninja.com',
+#                 'subject': 'New regex learning path!',
+#                 'date': 'Sun, 18 Aug 2019 17:16:10'}
+#     assert actual == expected
+
+#     assert get_email_details('bogus') is None
+
+
+# if __name__ == '__main__':
+#     main()
