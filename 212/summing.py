692e3794cde482a4618be2bc74c3bec919423c26
nishant hegde <nhegde@netflix.com>
2019-09-30 05:23:59 -0700

    kata submissionm 930

--- 212/summing.py
+++ 212/summing.py
@@ -1,20 +1,35 @@
 from contextlib import suppress
+import inspect
 
 
 def sum_numbers(numbers):
-    """This generator divides each nummber by its consecutive number.
-       So if it gets passed in [4, 2, 1] it yields 4/2 and 2/1.
-       It ignores ZeroDivisionError and TypeError exceptions (latter happens
-       when a string or other non-numeric data type is in numbers)
 
-       Task: use contextlib's suppress twice to make the code below more concise.
-    """
-    for i, j in zip(numbers, numbers[1:]):
-        # replace the block below
-        try:
-            try:
-                yield i/j
-            except ZeroDivisionError:
-                pass
-        except TypeError:
-            pass
+  for i, j in zip(numbers, numbers[1:]):
+
+    with suppress(ZeroDivisionError):
+      with suppress(TypeError):
+        yield i / j
+
+
+# def main():
+
+#   print('here ...')
+
+#   numbers = [1, 2, 0, 4, 5, 12, 'a', 3]
+#   actual = sum_numbers(numbers)
+#   print(actual)
+# #   expected = [0.5, 0.0, 0.8, 0.4166666666666667]
+# #   assert actual == expected
+
+# #   src = inspect.getsource(sum_numbers)
+# #   assert 'try' not in src
+# #   assert 'except ' not in src
+# #   assert 'yield' in src
+# #   assert 'TypeError' in src
+# #   assert 'ZeroDivisionError' in src
+# #   assert src.count('suppress(') in (1, 2)
+# #   assert src.count('with') in (1, 2)
+
+
+# if __name__ == '__main__':
+#   main()
