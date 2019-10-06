572cbaf37398a83f78ffec9a01e4b077ef883973
nishant hegde <nhegde@netflix.com>
2019-10-06 06:57:32 -0700

    kata submission 1006

--- 188/stats.py
+++ 188/stats.py
@@ -1,9 +1,12 @@
 from os import path
 import statistics as st
 from urllib.request import urlretrieve
+import os
 
+# local = os.getcwd()
+local = 'tmp'
+STATS = path.join(local, 'testfiles_number_loc.txt')
 
-STATS = path.join('/tmp', 'testfiles_number_loc.txt')
 if not path.isfile(STATS):
     urlretrieve('https://bit.ly/2Jp5CUt', STATS)
 
@@ -29,7 +32,9 @@ def get_all_line_counts(data: str = STATS) -> list:
     """Get all 186 line counts from the STATS file,
        returning a list of ints"""
     # TODO 1: get the 186 ints from downloaded STATS file
-    pass
+
+    with open(data, 'r') as f:
+        return [int(l.split()[0].strip()) for l in f.readlines()]
 
 
 def create_stats_report(data=None):
@@ -42,15 +47,32 @@ def create_stats_report(data=None):
 
     # TODO 2: complete this dict, use data list and
     # for the last 3 sample_ variables, use sample list
-    stats = dict(count=None,
-                 min_=None,
-                 max_=None,
-                 mean=None,
-                 pstdev=None,
-                 pvariance=None,
-                 sample_count=None,
-                 sample_stdev=None,
-                 sample_variance=None,
+    stats = dict(count=len(data),
+                 min_=min(data),
+                 max_=max(data),
+                 mean=round(st.mean(data), 2),
+                 pstdev=round(st.pstdev(data), 2),
+                 pvariance=round(st.pvariance(data), 2),
+                 sample_count=round(len(sample), 2),
+                 sample_stdev=round(st.stdev(sample), 2),
+                 sample_variance=round(st.variance(sample), 2),
                  )
 
     return STATS_OUTPUT.format(**stats)
+
+
+# def main():
+
+#     print('here ...')
+#     counts = list(get_all_line_counts())
+#     assert len(counts) == 186
+#     assert all(isinstance(c, int) for c in counts)
+#     assert sum(counts) == 8135
+#     # print(get_all_line_counts())
+#     # print(len(get_all_line_counts()))
+
+#     print(create_stats_report())
+
+
+# if __name__ == '__main__':
+#     main()
