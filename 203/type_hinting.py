e6f091deb1b81a1a294467cbb211dd135398a435
nishant hegde <nhegde@netflix.com>
2019-10-08 05:34:13 -0700

    kata submission 1008

--- 203/type_hinting.py
+++ 203/type_hinting.py
@@ -9,15 +9,15 @@ class Employee:
     :param weekly_pay: Property which returns a string for weekly pay
     """
 
-    def __init__(self, first_name, last_name, days_per_week,
-                 hours_per_day, wage):
+    def __init__(self, first_name: str, last_name: str, days_per_week: int,
+                 hours_per_day: int, wage: float) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.days_per_week = days_per_week
         self.hours_per_day = hours_per_day
         self.wage = wage
 
-    def _rounder(self, number, places):
+    def _rounder(self, number: float, places: int) -> str:
         """Rounds a number the specified number of places
 
         :param number: Float of number of round
@@ -28,7 +28,7 @@ class Employee:
         return f"${amount:0.2f}"
 
     @property
-    def weekly_pay(self):
+    def weekly_pay(self) -> str:
         """Returns amount of weekly pay in US currency
 
         For instance: $250.75
@@ -41,3 +41,4 @@ class Employee:
 if __name__ == "__main__":
     coder = Employee("Joe", "Blow", 5, 8, 18.0)
     print(coder.weekly_pay)
+    # print(type(coder.weekly_pay))
