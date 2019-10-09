e363f89904147e748f900bd750a6a6851cb30d7b
nishant hegde <nhegde@netflix.com>
2019-10-09 09:06:17 -0700

    initiate kata

---
+++ 97/holidays.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+import os
+from urllib.request import urlretrieve
+
+from bs4 import BeautifulSoup
+
+
+# prep data
+holidays_page = os.path.join('/tmp', 'us_holidays.php')
+urlretrieve('https://bit.ly/2LG098I', holidays_page)
+
+with open(holidays_page) as f:
+    content = f.read()
+
+holidays = defaultdict(list)
+
+
+def get_us_bank_holidays(content=content):
+    """Receive scraped html output, make a BS object, parse the bank
+       holiday table (css class = list-table), and return a dict of
+       keys -> months and values -> list of bank holidays"""
+    pass
