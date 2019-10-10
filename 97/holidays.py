047aec550bb7fa174d2cf7837cc39e067670cd8a
nishant hegde <nhegde@netflix.com>
2019-10-10 10:46:36 -0700

    kata submission 1010

--- 97/holidays.py
+++ 97/holidays.py
@@ -2,11 +2,12 @@ from collections import defaultdict
 import os
 from urllib.request import urlretrieve
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as Soup
 
-
+# local = os.getcwd()
+local = '/tmp'
 # prep data
-holidays_page = os.path.join('/tmp', 'us_holidays.php')
+holidays_page = os.path.join(local, 'us_holidays.php')
 urlretrieve('https://bit.ly/2LG098I', holidays_page)
 
 with open(holidays_page) as f:
@@ -19,4 +20,26 @@ def get_us_bank_holidays(content=content):
     """Receive scraped html output, make a BS object, parse the bank
        holiday table (css class = list-table), and return a dict of
        keys -> months and values -> list of bank holidays"""
-    pass
+    soup = Soup(content, 'html.parser')
+
+    table = soup.find('table', attrs={'class': 'list-table'})
+    table_body = table.find('tbody')
+
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        holidays[cols[1][5:7]].append(cols[3])
+
+    return holidays
+
+
+# def main():
+#     print('here ...')
+#     ret = get_us_bank_holidays()
+#     # print(type(ret))
+#     print(ret.get("11"))
+
+
+# if __name__ == '__main__':
+#     main()
