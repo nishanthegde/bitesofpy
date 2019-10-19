409c0dbd3201aeb5b30b43940fe75bbdc0d0d251
nishant hegde <nhegde@netflix.com>
2019-10-19 15:42:42 -0700

    kata submission 1019

--- 226/python_news.py
+++ 226/python_news.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 
+
 from bs4 import BeautifulSoup
 import requests
+import re
 
 # feed = https://news.python.sc/, to get predictable results we cached
 # first two pages - use these:
@@ -9,6 +11,11 @@ import requests
 # https://bites-data.s3.us-east-2.amazonaws.com/news.python.sc/index2.html
 
 Entry = namedtuple('Entry', 'title points comments')
+
+homepage = ("https://bites-data.s3.us-east-2.amazonaws.com/"
+            "news.python.sc/index.html")
+page2 = ("https://bites-data.s3.us-east-2.amazonaws.com/"
+         "news.python.sc/index2.html")
 
 
 def _create_soup_obj(url):
@@ -25,4 +32,37 @@ def get_top_titles(url, top=5):
     """
     soup = _create_soup_obj(url)
 
-    # you code
+    titles = []
+    points = []
+    comments = []
+
+    points_pattern = re.compile(r'^(\d+)\spoint')
+    comments_pattern = re.compile(r'\|\s(\d+)\scomment')
+
+    for t in soup.find_all('span', attrs={'class': 'title'}):
+        titles.append(t.text.strip())
+
+    for c in soup.find_all('span', attrs={'class': 'controls'}):
+        for s in c.find_all('span', attrs={'class': 'smaller'}):
+            # controls.append(s.text.strip().split('\n')[0])
+            pt = points_pattern.search(s.text.strip().split('\n')[0])
+            points.append(int(pt.group(1)))
+            com = comments_pattern.search(s.text.strip().split('\n')[-1])
+            comments.append(int(com.group(1)))
+            # comments.append(com.group(1))
+
+    all_entries = sorted(list(zip(titles, points, comments)), key=lambda x: (x[1], x[2]), reverse=True)
+
+    return [Entry(title=e[0], points=e[1], comments=e[2]) for e in all_entries][:top]
+
+
+# def main():
+#     print('here ...')
+
+#     actual = get_top_titles(homepage)
+#     actual = get_top_titles(page2, 2)
+#     print(actual)
+
+
+# if __name__ == '__main__':
+#     main()
