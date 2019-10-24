bea984944dde45dc7c038831c7c831afaaa97079
nishant hegde <nhegde@netflix.com>
2019-10-24 11:51:59 -0700

    kata submission 1024

--- 130/cars.py
+++ 130/cars.py
@@ -4,20 +4,40 @@ import requests
 
 CAR_DATA = 'https://bit.ly/2Ov65SJ'
 
-# pre-work: load JSON data into program
-
 with requests.Session() as s:
     data = s.get(CAR_DATA).json()
 
 
-# your turn:
-def most_prolific_automaker(year):
+def most_prolific_automaker(year: int) -> str:
     """Given year 'year' return the automaker that released
        the highest number of new car models"""
-    pass
+    year_models = [d['automaker'] for d in data if d['year'] == year]
+    c = Counter(year_models)
+    return c.most_common(1)[0][0]
 
 
-def get_models(automaker, year):
+def get_models(automaker: str, year: int) -> set:
     """Filter cars 'data' by 'automaker' and 'year',
        return a set of models (a 'set' to avoid duplicate models)"""
-    pass
+    models = set([d['model'] for d in data if d['automaker'] == automaker and d['year'] == year])
+
+    return models
+
+
+# def main():
+#     print('focus ...')
+#     # print(len(data))
+#     print(most_prolific_automaker(1999))
+#     print(most_prolific_automaker(2008))
+#     print(most_prolific_automaker(2013))
+
+#     models = get_models('Volkswagen', 2008)
+#     print(models)
+
+#     print(get_models('Nissan', 2000))
+#     print(get_models('Opel', 2008))
+#     print(get_models('Mercedes-Benz', 2007))
+
+
+# if __name__ == '__main__':
+#     main()
