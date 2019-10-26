769b4eb7f428acf6c41317f6590e9e5d81123705
nishant hegde <nhegde@netflix.com>
2019-10-26 13:09:08 -0700

    kata submission 1026

--- 111/ipinfo.py
+++ 111/ipinfo.py
@@ -3,7 +3,19 @@ import requests
 IPINFO_URL = 'http://ipinfo.io/{ip}/json'
 
 
-def get_ip_country(ip_address):
+def get_ip_country(ip_address: str) -> str:
     """Receives ip address string, use IPINFO_URL to get geo data,
        parse the json response returning the country code of the IP"""
-    pass
+    resp = requests.get(IPINFO_URL.format(ip=ip_address))
+
+    return eval(resp.text)['country']
+
+
+# def main():
+#     print('...thanks...')
+#     print(get_ip_country('187.190.38.36'))
+#     print(get_ip_country('185.161.200.10'))
+
+
+# if __name__ == '__main__':
+#     main()
