f2ebd4d429cf15d52f1ca4781f2faa381f07f03b
nishant hegde <nhegde@netflix.com>
2019-10-28 06:54:54 -0700

    kata submission 1028

--- 43/kwargs.py
+++ 43/kwargs.py
@@ -1,1 +1,17 @@
-def get_profile(...
+def get_profile(*, name: str ='julian', profession: str ='programmer') -> str:
+    return '{} is a {}'.format(name, profession)
+
+
+# def main():
+#     print('have fun...')
+#     print(get_profile())
+#     # print(get_profile('julian'))
+#     # print(get_profile(test=True))
+#     # print(get_profile(name='bob', profession='software developer',
+#     #                   another_flag=False))
+#     # print(get_profile(name='bob'))
+#     print(get_profile(name='bob', profession='software developer'))
+
+
+# if __name__ == '__main__':
+#     main()
