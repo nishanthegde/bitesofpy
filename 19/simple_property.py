0772db3ea660a9d141fdf228fd70fc7716123daa
nishant hegde <nhegde@netflix.com>
2019-11-02 07:12:31 -0700

    kata submission 1102

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -1,7 +1,36 @@
-from datetime import datetime
+import inspect
+from datetime import datetime, timedelta
 
 NOW = datetime.now()
 
 
 class Promo:
-    pass
+
+    def __init__(self, name: str, expires: datetime):
+        self.name = name
+        self.expires = expires
+
+    @property
+    def expired(self):
+        """Get the current voltage."""
+        return self.expires < NOW
+
+
+# def main():
+#     print('thank you for everything ...')
+#     past_time = NOW - timedelta(seconds=3)
+#     # print(past_time)
+#     # print(type(past_time))
+
+#     twitter_promo = Promo('twitter', past_time)
+#     assert twitter_promo.expired
+
+#     future_date = NOW + timedelta(days=1)
+#     newsletter_promo = Promo('newsletter', future_date)
+#     assert not newsletter_promo.expired
+
+#     assert 'property' in inspect.getsource(Promo)
+
+
+# if __name__ == '__main__':
+#     main()
