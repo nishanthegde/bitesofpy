a45410b87ba362cf7bc927d5393dca4b7085acc7
nishant hegde <nhegde@netflix.com>
2019-11-06 10:34:58 -0800

    kata submission 11

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,24 @@
-def fizzbuzz(num):
-    pass
+def fizzbuzz(num: int):
+
+    ret = []
+
+    if num % 3 == 0 and num != 0:
+        ret.append('Fizz')
+
+    if num % 5 == 0 and num != 0:
+        ret.append('Buzz')
+
+    if ret:
+        return ' '.join(ret)
+    else:
+        return num
+
+
+# def main():
+#     # print('thank you...')
+#     for i in range(0, 17):
+#         print(fizzbuzz(i))
+
+
+# if __name__ == '__main__':
+#     main()
