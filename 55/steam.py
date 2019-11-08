3a2d4a350e8b079ce9f42b377483d11c3e99a623
nishant hegde <nhegde@netflix.com>
2019-11-07 17:22:00 -0800

    kata submission 1107

--- 55/steam.py
+++ 55/steam.py
@@ -8,6 +8,33 @@ FEED_URL = "http://bit.ly/2IkFe9B"
 Game = namedtuple('Game', 'title link')
 
 
-def get_games():
+def get_games() -> list:
     """Parses Steam's RSS feed and returns a list of Game namedtuples"""
-    pass
+    d = feedparser.parse(FEED_URL)
+
+    games = []
+
+    for i in range(len(d['entries'])):
+        title = d['entries'][i]['title']
+        link = d['entries'][i]['link']
+
+        games.append(Game(title=title, link=link))
+
+    return games
+
+# def main():
+#     print('zone in...')
+#     games = get_games()
+#     assert len(games) == 30
+#     assert all(isinstance(game, tuple) for game in games)
+#     assert all('store.steampowered.com' in game.link for game in games)
+#     first_game = games[0]
+#     assert first_game.title == 'Midweek Madness - RiME, 33% Off'
+#     assert first_game.link == 'http://store.steampowered.com/news/31695/'
+#     last_game = games[-1]
+#     assert last_game.title == 'Now Available on Steam - Loco Dojo, 35% off!'
+#     assert last_game.link == 'http://store.steampowered.com/news/31113/'
+
+
+# if __name__ == '__main__':
+#     main()
