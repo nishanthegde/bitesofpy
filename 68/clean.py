7471e2393e684d484a403b973585b72e64a61757
nishant hegde <nhegde@netflix.com>
2019-11-12 09:26:31 -0800

    kata submission 1112

--- 68/clean.py
+++ 68/clean.py
@@ -1,3 +1,18 @@
-def remove_punctuation(input_string):
+import string
+
+exclude = set(string.punctuation)
+
+
+def remove_punctuation(input_string: str)->str:
     """Return a str with punctuation chars stripped out"""
-    pass
+    # table = str.maketrans(dict.fromkeys(string.punctuation))
+    return ''.join(c for c in input_string if c not in exclude)
+
+
+# def main():
+#     print('thank you for everything ...')
+#     print(remove_punctuation("Some other (chars) |:-^, let's delete them"))
+
+
+# if __name__ == '__main__':
+#     main()
