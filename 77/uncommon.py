0781ada14c972187291bba23bb56200a30823802
nishant hegde <nhegde@netflix.com>
2019-11-16 05:47:21 -0800

    kata submission 1116

--- 77/uncommon.py
+++ 77/uncommon.py
@@ -1,4 +1,23 @@
-def uncommon_cities(my_cities, other_cities):
+def uncommon_cities(my_cities: list, other_cities: list) -> int:
     """Compare my_cities and other_cities and return the number of different
        cities between the two"""
-    pass
+    return len(set(my_cities) ^ set(other_cities))
+
+
+# def main():
+#     print('thank you for the curiosity ...')
+
+#     my_cities = ['Rome', 'Paris', 'Madrid', 'Chicago', 'Seville', 'Berlin']
+#     other_cities = ['Paris', 'Boston', 'Sydney', 'Madrid', 'Moscow', 'Lima']
+
+#     my_cities = ['Rome', 'Paris', 'Madrid', 'Chicago', 'Seville', 'Berlin']
+#     other_cities = ['Rome', 'Paris', 'Madrid', 'Chicago', 'Seville', 'Berlin']
+
+#     my_cities = ['Rome', 'Paris', 'Madrid']
+#     other_cities = ['Chicago', 'Seville', 'Berlin']
+
+#     print(uncommon_cities(my_cities, other_cities))
+
+
+# if __name__ == '__main__':
+#     main()
