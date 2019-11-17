eea0f4084b5c73fb920c09b0a398e0f9108a3158
nishant hegde <nhegde@netflix.com>
2019-11-17 11:50:39 -0800

    kata submission 1117

--- 110/division.py
+++ 110/division.py
@@ -5,4 +5,24 @@ def divide_numbers(numerator, denominator):
        ValueError reraise it. Following do the division and return the result.
        However if denominator is 0 catch the corresponding exception Python
        throws (cannot divide by 0), and return 0"""
-    pass
+    try:
+        n = int(numerator)
+        d = int(denominator)
+    except ValueError:
+        print('Caught ValueError Exception')
+        raise ValueError('args must be convertable to int')
+    else:
+        try:
+            return n / d
+        except ZeroDivisionError:
+            print('Caught ZeroDivisionError Exception')
+            return 0
+
+
+# def main():
+#     print('thank you for the ocean ...')
+#     print(divide_numbers(10, 0))
+
+
+# if __name__ == '__main__':
+#     main()
