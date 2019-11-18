01b8f10ab17878093c7ab7aa600bca9c9d4c3f67
nishant hegde <nhegde@netflix.com>
2019-11-18 10:08:30 -0800

    kata submission 1118

--- 115/indents.py
+++ 115/indents.py
@@ -1,3 +1,12 @@
-def count_indents(text):
+def count_indents(text: str) -> int:
     """Takes a string and counts leading white spaces, return int count"""
-    pass
+    return len(text) - len(text.lstrip(' '))
+
+
+# def main():
+#     print('thank you for everything...')
+#     print(count_indents('    string'))
+
+
+# if __name__ == '__main__':
+#     main()
