b02077f0b12c074b4c3914ea56d061676235f77c
nishant hegde <nhegde@netflix.com>
2019-11-21 10:34:06 -0800

    kata submission 1121

--- 181/order.py
+++ 181/order.py
@@ -7,7 +7,25 @@ class OrderedList:
         self._numbers = []
 
     def add(self, num):
-        # you complete
+        pos = bisect.bisect_left(self._numbers, num)
+        self._numbers.insert(pos, num)
 
     def __str__(self):
         return ', '.join(str(num) for num in self._numbers)
+
+
+# def main():
+#     print('thank you for everything...')
+#     order = OrderedList()
+#     order.add(10)
+#     print(order)
+#     order.add(1)
+#     print(order)
+#     order.add(16)
+#     print(order)
+#     order.add(5)
+#     print(order)
+
+
+# if __name__ == '__main__':
+#     main()
