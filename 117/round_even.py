008b2eb39538c4ab2681f58c4c72d879b54189c4
nishant hegde <nhegde@netflix.com>
2019-11-25 12:33:38 -0800

    kata submission 1125b

--- 117/round_even.py
+++ 117/round_even.py
@@ -1,3 +1,19 @@
-def round_even(number):
+def round_even(number: float) -> int:
     """Takes a number and returns it rounded even"""
-    pass
+    mid = False
+    if round(number) - number == 0.5:
+        mid = True
+
+    if mid and round(number) % 2 != 0:
+        return int(number)
+    else:
+        return round(number)
+
+
+# def main():
+#     print('thank you for everything...')
+#     print(round_even(1.6))
+
+
+# if __name__ == '__main__':
+#     main()
