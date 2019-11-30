29b8d4b396acad59b9e3baa8c3c405c102271009
nishant hegde <nhegde@netflix.com>
2019-11-30 06:50:57 -0800

    kata submission 1130

--- 113/non_ascii.py
+++ 113/non_ascii.py
@@ -1,3 +1,20 @@
-def extract_non_ascii_words(text):
+def _is_ascii(s: str)-> bool:
+    return all(ord(c) < 128 for c in s)
+
+
+def extract_non_ascii_words(text: str) -> list:
     """Filter a text returning a list of non-ascii words"""
-    pass
+    words = text.split()
+    return [w for w in words if not _is_ascii(w)]
+
+
+# def main():
+#     print('thank you for everything ...')
+#     # print(_is_ascii('saber'))
+#     print(extract_non_ascii_words('This string only contains ascii words'))
+#     print(extract_non_ascii_words('Sí ... habrá que saber algo de Unicode, ¿no?'))
+#     print(extract_non_ascii_words('Over \u0e55\u0e57 57 flavours'))
+
+
+# if __name__ == '__main__':
+#     main()
