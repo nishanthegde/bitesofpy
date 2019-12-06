950f1b2b40ef0286e58c746b4b368721a8d2f09a
nishant hegde <nhegde@netflix.com>
2019-12-06 10:36:23 -0800

    kata submission 1206

--- 74/bday.py
+++ 74/bday.py
@@ -1,6 +1,20 @@
 import calendar
+from datetime import date
 
 
-def weekday_of_birth_date(date):
+def weekday_of_birth_date(date: date) ->str:
     """Takes a date object and returns the corresponding weekday string"""
-    pass
+    return date.strftime('%A')
+
+
+# def main():
+#     print('thank you for these problems...')
+#     dt = date(1974, 11, 11)
+#     dt = date(1978, 9, 8)
+#     dt = date(1946, 12, 28)
+#     # assert weekday_of_birth_date(dt) == 'Monday'
+#     print(weekday_of_birth_date(dt))
+
+
+# if __name__ == '__main__':
+#     main()
