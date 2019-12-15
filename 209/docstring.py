62c52d53a79384c8a29303b7a11f71cf9553b69d
nishant hegde <nhegde@netflix.com>
2019-12-15 12:13:39 -0800

    kata submit 1215

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,3 +1,20 @@
 def sum_numbers(numbers):
-    """Write your docstring here ..."""
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    ...
+    :raises: TypeError: if not all numeric values passed in
+    ...
+    :return: sum of numbers
+    :rtype: int
+    """
     pass
+
+
+def main():
+    print('thank you for everything...')
+
+
+if __name__ == '__main__':
+    main()
