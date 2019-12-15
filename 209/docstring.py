b0bf55aa2477cd5214f52640de12df11b529cc67
nishant hegde <nhegde@netflix.com>
2019-12-15 12:22:14 -0800

    kata submission 1215b

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,12 +1,11 @@
 def sum_numbers(numbers):
     """Sums numbers
 
-    :param numbers: a list of numbers
-    :type numbers: list
-    :raises: TypeError: if not all numeric values passed in
-    :return: sum of numbers
-    :rtype: int
-    """
+        :param numbers: a list of numbers
+        :type numbers: list
+        :raises: TypeError: if not all numeric values passed in
+        :return: sum of numbers
+        :rtype: int"""
     pass
 
 
