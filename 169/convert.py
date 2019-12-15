0de6229b0af84bbb4f93326360b953ebc60b0c3b
nishant hegde <nhegde@netflix.com>
2019-12-15 12:45:05 -0800

    kata submission 1215c

--- 169/convert.py
+++ 169/convert.py
@@ -5,4 +5,25 @@ def convert(value: float, fmt: str) -> float:
     :param fmt: String indicating format to convert to
     :return: Float rounded to 4 decimal places after conversion
     """
-    pass
+    if not isinstance(value, (int, float)):
+        raise TypeError('value must be int or float')
+
+    if fmt.lower() not in ['cm', 'in']:
+        raise ValueError('fmt must be in or cm')
+
+    if fmt.lower() == 'cm':
+        return round(value * 2.54, 4)
+    else:
+        return round(value * 0.393701, 4)
+
+
+# def main():
+#     print('thank you for everything...')
+#     # convert(-153.67, "cm")
+#     # convert(300, "cm")
+#     print(convert(60.5, "CM"))
+#     print(convert(83, "CM"))
+#     print(convert(91, "IN"))
+
+# if __name__ == '__main__':
+#     main()
