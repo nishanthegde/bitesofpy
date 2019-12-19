921472171d7dadd6d501ced262fdd8f0a3b886ae
nishant hegde <nhegde@netflix.com>
2019-12-19 07:58:41 -0800

    kata submission 1219

--- 155/split.py
+++ 155/split.py
@@ -1,13 +1,29 @@
-def split_words_and_quoted_text(text):
-    """Split string text by space unless it is
-       wrapped inside double quotes, returning a list
-       of the elements.
+import shlex
 
-       For example
-       if text =
-       'Should give "3 elements only"'
 
-       the resulting list would be:
-       ['Should', 'give', '3 elements only']
-    """
-    pass
+def split_words_and_quoted_text(text: str)-> list:
+  """Split string text by space unless it is
+     wrapped inside double quotes, returning a list
+     of the elements.
+
+     For example
+     if text =
+     'Should give "3 elements only"'
+
+     the resulting list would be:
+     ['Should', 'give', '3 elements only']
+  """
+  return shlex.split(text)
+
+
+# def main():
+#   print('thank you for everything you have given me...')
+#   print(split_words_and_quoted_text('Should give "3 words only"'))
+#   print(split_words_and_quoted_text('Our first program was "Hello PyBites"'))
+#   print(split_words_and_quoted_text('Because "Hello World" is really cliche'))
+#   print(split_words_and_quoted_text(('PyBites is a "A Community that Masters '
+#                                     'Python through Code Challenges"'))
+
+
+# if __name__ == '__main__':
+#   main()
