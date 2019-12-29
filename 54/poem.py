f819cfafba711a417e68e711c1d108df611c27fa
nishant hegde <nhegde@netflix.com>
2019-12-29 15:17:24 -0800

    kata submission 1229

--- 54/poem.py
+++ 54/poem.py
@@ -12,15 +12,48 @@ rosetti_unformatted = """
                       Only remember me; you understand
                       """
 
+shakespeare_unformatted = """
+                          To be, or not to be, that is the question:
+                          Whether 'tis nobler in the mind to suffer
+
+                          The slings and arrows of outrageous fortune,
+                          Or to take Arms against a Sea of troubles,
+                          """
+
 
 def print_hanging_indents(poem: str):
-    print(poem)
+
+  formatted = ''
+
+  lines = [l.strip() for l in poem.rstrip().splitlines()]
+  print(lines)
+
+  idx_no_indent = list()
+
+  for i in range(len(lines)):
+    if lines[i] == '':
+      idx_no_indent.append(i + 1)
+
+  print(idx_no_indent)
+
+  lines_indented = ['{}{}'.format(INDENTS * ' ', l) for l in lines]
+
+  lines_formatted = list()
+
+  for i in range(len(lines_indented)):
+    if i in idx_no_indent:
+      lines_formatted.append(lines_indented[i].lstrip())
+    else:
+      lines_formatted.append(lines_indented[i])
+
+  print('\n'.join([l for l in lines_formatted if l.strip() != '']))
 
 
-def main():
-    print('thank you ...')
-    print_hanging_indents(rosetti_unformatted)
+# def main():
+#   print('thank you for letting my mama have a great time...')
+#   print_hanging_indents(rosetti_unformatted)
+#   print_hanging_indents(shakespeare_unformatted)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#   main()
