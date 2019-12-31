4d1b00732192312c63e0a9d0a02a921c3047b355
nishant hegde <nhegde@netflix.com>
2019-12-31 11:42:13 -0800

    kata submission 1231a

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -4,3 +4,11 @@ from fibonacci import fib
 def test_first_two():
     assert fib(0) == 0
     assert fib(1) == 1
+
+
+def test_two():
+    assert fib(2) == 1
+
+
+def test_thirteen():
+    assert fib(13) == 233
