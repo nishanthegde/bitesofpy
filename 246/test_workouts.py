04f9b215958b11098f4edff56d40f06174f246f0
nishant hegde <nhegde@netflix.com>
2020-01-02 13:43:49 -0800

    kata submission 0102a

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -1,7 +1,19 @@
+import sys
 import pytest
 
 from workouts import print_workout_days
 
 
-def test_print_workout_days():
-    pass
+def test_print_workout_days(capfd):
+    print_workout_days('Upper')
+    captured = capfd.readouterr()
+    assert captured.out == 'Mon, Thu\n'
+
+
+# def main():
+#     print('thank you for this life... help me find a way to give back, do seva...')
+#     test_print_workout_days(sys.stdout)
+
+
+# if __name__ == '__main__':
+#     main()
