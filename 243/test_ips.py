71360893fca8ca80fc860e21141368344f344c96
nishant hegde <nhegde@netflix.com>
2020-01-06 15:10:43 -0800

    kata submission 0106b

--- 243/test_ips.py
+++ 243/test_ips.py
@@ -9,8 +9,8 @@ from ips import (ServiceIPRange, parse_ipv4_service_ranges,
                  get_aws_service_range)
 
 URL = "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
-local = os.getcwd()
-# local = '/tmp'
+# local = os.getcwd()
+local = '/tmp'
 # TMP = os.getenv("TMP", "/tmp")
 TMP = os.getenv("TMP", local)
 PATH = Path(TMP, "ip-ranges.json")
