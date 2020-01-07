fd4e73b8f82eef8ef7a824bb550388bcd05fb566
nishant hegde <nhegde@netflix.com>
2020-01-06 16:56:36 -0800

    0106

--- 209/docstring.py
+++ 209/docstring.py
@@ -1,8 +1,8 @@
 def sum_numbers(numbers):
     """Sums numbers
-
         :param numbers: a list of numbers
         :type numbers: list
+        :raises TypeError: if not all numeric values passed in
         :return: sum of numbers
         :rtype: int
         """
