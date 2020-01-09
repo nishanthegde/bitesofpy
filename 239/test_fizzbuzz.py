af3409d803a4f9d7ef981633bf32341625f12bc8
nishant hegde <nhegde@netflix.com>
2020-01-09 09:59:53 -0800

    kata submission 0109

--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -1,3 +1,30 @@
+import pytest
 from fizzbuzz import fizzbuzz
 
 # write one or more pytest functions below, they need to start with test_
+
+
+def test_fizzbuzz():
+    assert fizzbuzz(15) == 'Fizz Buzz'
+
+
+def test_buzz():
+    assert fizzbuzz(5) == 'Buzz'
+
+
+def test_fizz():
+    assert fizzbuzz(3) == 'Fizz'
+
+
+def test_other1():
+    assert fizzbuzz(89) == 89
+
+
+def test_other2():
+    assert fizzbuzz(True) == True
+
+
+def test_other3():
+    with pytest.raises(TypeError) as exc:
+        fizzbuzz('nishant')
+        assert 'not all arguments converted during string formatting' in str(exc)
