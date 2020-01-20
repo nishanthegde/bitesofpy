09cb13b95f6bb9e3aebe6bdc2cb6850e1413b5cb
nishant hegde <nhegde@netflix.com>
2020-01-20 09:32:06 -0800

    kata submission 0120

--- 254/scoping.py
+++ 254/scoping.py
@@ -5,4 +5,22 @@ def sum_numbers(numbers: list) -> int:
     """Sums passed in numbers returning the total, also
        update the global variable num_hundreds with the amount
        of times 100 fits in total"""
-    pass
+    global num_hundreds
+
+    num_hundreds += sum(numbers) // 100
+    print(num_hundreds)
+    return sum(numbers)
+
+
+def main():
+    print('thank you for everything you have given me...')
+    print(sum_numbers([]))
+    print(sum_numbers([1, 2, 3]))
+    print(sum_numbers([40, 50, 60]))
+    print(sum_numbers([140, 50, 60]))
+    print(sum_numbers([140, 150, 160]))
+    print(sum_numbers([1140, 150, 160]))
+
+
+if __name__ == '__main__':
+    main()
