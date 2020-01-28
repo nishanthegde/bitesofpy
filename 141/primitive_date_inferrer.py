12795d3ee02cb9f5d6f91fbaa96d47b50a244cfc
nishant hegde <nhegde@netflix.com>
2020-01-28 15:32:03 -0800

    kata submit 0128

--- 141/primitive_date_inferrer.py
+++ 141/primitive_date_inferrer.py
@@ -14,7 +14,7 @@ class DateFormat(Enum):
         """ Arg:
         val(int | None) enum member value
         Returns:
-        1. for val=None a list of explicit format strings 
+        1. for val=None a list of explicit format strings
             for all supported date formats in this enum
         2. for val=n an explicit format string for a given enum member value
         """
@@ -43,7 +43,7 @@ def _maybe_DateFormats(date_str):
     maybe_formats = []
     for idx, d_parse_fmt in enumerate(d_parse_formats):
         try:
-            _parsed_date = datetime.strptime(date_str, d_parse_fmt) # pylint: disable=W0612
+            _parsed_date = datetime.strptime(date_str, d_parse_fmt)  # pylint: disable=W0612
             maybe_formats.append(DateFormat(idx))
         except ValueError:
             pass
@@ -52,7 +52,7 @@ def _maybe_DateFormats(date_str):
     return maybe_formats
 
 
-def get_dates(dates):
+def get_dates(dates: list) -> list:
     """ Args:
     dates (list) list of date strings
     where each list item represents a date in unknown format
@@ -63,4 +63,63 @@ def get_dates(dates):
     Alowed/supported date formats are defined in a DF enum class.
     """
     # complete this method
-    pass
+    date_return = list()
+    freq = dict()
+    d_parse_formats = DateFormat.get_d_parse_formats()
+
+    for d in dates:
+        formats = _maybe_DateFormats(d)
+        for f in formats:
+            if f not in freq:
+                freq[f] = 1
+            else:
+                freq[f] += 1
+
+    most_freq = max(freq.values())
+    most_freq_keys = list()
+
+    for k, v in freq.items():
+        if v == most_freq:
+            most_freq_keys.append(k)
+
+    if len(most_freq_keys) > 1:
+        # tie
+        raise InfDateFmtError
+    elif most_freq_keys[0].value == -999:
+        raise InfDateFmtError
+    else:
+        # elif most_freq_keys[0].value == 1:
+        for d in dates:
+            try:
+                date_return.append(datetime.strftime(datetime.strptime(d, d_parse_formats[most_freq_keys[0].value]), "%Y-%m-%d"))
+            except:
+                date_return.append("Invalid")
+    # else:
+    #     return freq
+
+    return date_return
+
+
+def main():
+    print('thank you for everything...')
+
+    dates = [
+        "12/16/30",
+        "16/03/54",
+        "97/07/26",
+        "04/04/31",
+        "01/08/07",
+        "02/02/29",
+        "73/03/08",
+        "06/07/55",
+        "10/09/77",
+        "18/03/43",
+        "30/11/24",
+        "08/01/51",
+    ]
+
+    print(get_dates(dates))
+
+
+if __name__ == '__main__':
+    main()
