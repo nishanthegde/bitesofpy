04b93b1838d86da718fd40cf36c16b004ac1d7fb
nishant hegde <nhegde@netflix.com>
2020-02-27 09:40:59 -0800

    kata submission 0227

--- 98/grid.py
+++ 98/grid.py
@@ -1,3 +1,5 @@
+import test_grid as tg
+
 DOWN, UP, LEFT, RIGHT = '⇓', '⇑', '⇐', '⇒'
 START_VALUE = 1
 
@@ -7,4 +9,22 @@ def print_sequence_route(grid, start_coordinates=None):
        START_VALUE coordinates and move through the numbers in order printing
        them.  Each time you turn append the grid with its corresponding symbol
        (DOWN / UP / LEFT / RIGHT). See the TESTS for more info."""
-    pass
+
+    grid_list = [l for l in grid.splitlines() if l]
+
+    for i, l in enumerate(grid_list):
+        if ' {} '.format(START_VALUE) in l:
+            print(i, l)
+
+    # return grid_list
+
+
+def main():
+    print("thank you for everything...")
+    print(print_sequence_route(tg.small_grid))
+    print(print_sequence_route(tg.intermediate_grid))
+    print(print_sequence_route(tg.big_grid))
+
+
+if __name__ == "__main__":
+    main()
