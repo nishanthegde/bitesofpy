c5043a5a0ea4dce2cb2507e095bfa04ca3eb6d2c
nishant hegde <nhegde@netflix.com>
2020-02-28 17:40:49 -0800

    kata submission 0228

--- 98/grid.py
+++ 98/grid.py
@@ -1,4 +1,6 @@
 import test_grid as tg
+import re
+import numpy as np
 
 DOWN, UP, LEFT, RIGHT = '⇓', '⇑', '⇐', '⇒'
 START_VALUE = 1
@@ -10,20 +12,22 @@ def print_sequence_route(grid, start_coordinates=None):
        them.  Each time you turn append the grid with its corresponding symbol
        (DOWN / UP / LEFT / RIGHT). See the TESTS for more info."""
 
+    # print(grid)
+
     grid_list = [l for l in grid.splitlines() if l]
+    grid_list = [re.split(' - |    |   ||', r) for r in grid_list][::2]
+    grid_list = [[int(float(j)) for j in i] for i in grid_list]
 
-    for i, l in enumerate(grid_list):
-        if ' {} '.format(START_VALUE) in l:
-            print(i, l)
+    grid_mat = np.array(grid_list)
 
-    # return grid_list
+    return grid_mat
 
 
 def main():
     print("thank you for everything...")
-    print(print_sequence_route(tg.small_grid))
-    print(print_sequence_route(tg.intermediate_grid))
-    print(print_sequence_route(tg.big_grid))
+    print(print_sequence_route(tg.small_grid).shape)
+    print(type(print_sequence_route(tg.intermediate_grid)))
+    print(print_sequence_route(tg.big_grid).shape)
 
 
 if __name__ == "__main__":
