107e16d7ae8af34cac0549c112bb44f67b67de6d
nishant hegde <nhegde@netflix.com>
2020-03-06 15:13:08 -0800

    kata submission 0306

--- 98/grid.py
+++ 98/grid.py
@@ -1,12 +1,36 @@
 import test_grid as tg
 import re
-import numpy as np
+# import numpy as np
 
 DOWN, UP, LEFT, RIGHT = '⇓', '⇑', '⇐', '⇒'
 START_VALUE = 1
 
 
-def check_neighbors_val(grid_array: np.array, val: int, center: tuple) -> tuple:
+# def check_neighbors_val(grid_array: np.array, val: int, center: tuple) -> tuple:
+#     # check 4 neighbors for next value and return tuple of indices of new center and direction
+
+#     center_row_idx = center[0]
+#     center_col_idx = center[1]
+
+#     # print(grid_array.shape)
+
+#     if center_col_idx < grid_array.shape[1] - 1 and grid_array[center_row_idx, center_col_idx + 1] == val + 1:
+#         center_col_idx = center_col_idx + 1
+#         direction = RIGHT
+#     if center_col_idx > 0 and grid_array[center_row_idx, center_col_idx - 1] == val + 1:
+#         center_col_idx = center_col_idx - 1
+#         direction = LEFT
+#     if center_row_idx < grid_array.shape[0] - 1 and grid_array[center_row_idx + 1, center_col_idx] == val + 1:
+#         center_row_idx = center_row_idx + 1
+#         direction = DOWN
+#     if center_row_idx > 0 and grid_array[center_row_idx - 1, center_col_idx] == val + 1:
+#         center_row_idx = center_row_idx - 1
+#         direction = UP
+
+#     return (center_row_idx, center_col_idx, direction)
+
+
+def check_neighbors_val1(grid_list: list, val: int, center: tuple) -> tuple:
     # check 4 neighbors for next value and return tuple of indices of new center and direction
 
     center_row_idx = center[0]
@@ -14,16 +38,16 @@ def check_neighbors_val(grid_array: np.array, val: int, center: tuple) -> tuple:
 
     # print(grid_array.shape)
 
-    if center_col_idx < grid_array.shape[1] - 1 and grid_array[center_row_idx, center_col_idx + 1] == val + 1:
+    if center_col_idx < len(grid_list) - 1 and grid_list[center_row_idx][center_col_idx + 1] == val + 1:
         center_col_idx = center_col_idx + 1
         direction = RIGHT
-    if center_col_idx > 0 and grid_array[center_row_idx, center_col_idx - 1] == val + 1:
+    if center_col_idx > 0 and grid_list[center_row_idx][center_col_idx - 1] == val + 1:
         center_col_idx = center_col_idx - 1
         direction = LEFT
-    if center_row_idx < grid_array.shape[0] - 1 and grid_array[center_row_idx + 1, center_col_idx] == val + 1:
+    if center_row_idx < len(grid_list[0]) - 1 and grid_list[center_row_idx + 1][center_col_idx] == val + 1:
         center_row_idx = center_row_idx + 1
         direction = DOWN
-    if center_row_idx > 0 and grid_array[center_row_idx - 1, center_col_idx] == val + 1:
+    if center_row_idx > 0 and grid_list[center_row_idx - 1][center_col_idx] == val + 1:
         center_row_idx = center_row_idx - 1
         direction = UP
 
@@ -43,8 +67,10 @@ def print_sequence_route(grid, start_coordinates=None):
     # grid_list = [re.split(' - |\\s*|   ||', r) for r in grid_list][::2]
     grid_list = [[int(float(j)) for j in i] for i in grid_list]
 
-    grid_array = np.array(grid_list)
-    num_elements = (grid_array.shape[0] * grid_array.shape[1])
+    # print(grid_list)
+    # grid_array = np.array(grid_list)
+    # num_elements = (grid_array.shape[0] * grid_array.shape[1])
+    num_elements = len(grid_list)**2
 
     elements = [e for e in range(START_VALUE, num_elements)]
 
@@ -55,15 +81,25 @@ def print_sequence_route(grid, start_coordinates=None):
 
     for i, e in enumerate(elements):
 
-        start_idx_tuple = np.where(grid_array == e)
-        start_idx_coord = list(zip(start_idx_tuple[0], start_idx_tuple[1]))
+        # start_idx_tuple = np.where(grid_array == e)
+
+        # start_idx_coord = list(zip(start_idx_tuple[0], start_idx_tuple[1]))
 
         # assign row index and col index for START_VALUE to center
-        center_row_idx = start_idx_coord[0][0]
-        center_col_idx = start_idx_coord[0][1]
+        # center_row_idx = start_idx_coord[0][0]
+        # center_col_idx = start_idx_coord[0][1]
+
+        # print(center_row_idx, center_col_idx)
+
+        for k, v in enumerate(grid_list):
+            for j in range(0, len(v)):
+                if e == v[j]:
+                    # print(k, j)
+                    center_row_idx = k
+                    center_col_idx = j
 
         # get current direction
-        current_dir = check_neighbors_val(grid_array, e, (center_row_idx, center_col_idx))[2]
+        current_dir = check_neighbors_val1(grid_list, e, (center_row_idx, center_col_idx))[2]
 
         # get previous direction
         if i > 0:
@@ -85,13 +121,13 @@ def print_sequence_route(grid, start_coordinates=None):
         if i == (num_elements - 2):
             print((' '.join(map(str, grid_line[st_idx::2])) + ' ' + str(num_elements)).strip())
 
-    return grid_array
+    return grid_list
 
 
 def main():
     print("thank you for everything...")
-    # print(print_sequence_route(tg.very_small_grid))
-    # print(print_sequence_route(tg.small_grid))
+    print(print_sequence_route(tg.very_small_grid))
+    print(print_sequence_route(tg.small_grid))
     # print(print_sequence_route(tg.intermediate_grid))
     # print(print_sequence_route(tg.big_grid))
 
