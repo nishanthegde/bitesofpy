5b2a1f106486b631dfee86fe3c3681a7afff65ed
nishant hegde <nhegde@netflix.com>
2020-03-09 10:47:58 -0700

    kata submission 0308

--- 31/matmul.py
+++ 31/matmul.py
@@ -2,13 +2,13 @@ class Matrix(object):
 
     def __init__(self, values):
         if not isinstance(values, list):
-            raise ValueError("<Matrix object must be a list of lists of ints>")
-
-        # if (all(isinstance(item, int) for item in sub_list) for sub_list in values):
-        #     raise ValueError("<Matrix object must be a list of lists of ints>")
+            raise ValueError("<Matrix object must be a list of lists>")
         else:
-            print([(item for item in sub_list) for sub_list in values])
-            self.values = values
+            for sub_list in values:
+                for i in sub_list:
+                    if not isinstance(i, int):
+                        raise ValueError("<Matrix object must be a list of lists of ints>")
+        self.values = values
 
     def __repr__(self):
         return f'<Matrix values="{self.values}">'
@@ -19,10 +19,26 @@ class Matrix(object):
             for A to be able to be multiplied by B
             number of columns in A should be equal to number of rows in B
         """
+        product_matrix = []
         if len(self.values) > 0:
             if len(self.values[0]) == len(other.values):
-                # return f'<Matrices can be multiplied">'
-                return self.values[0][0] * other.values[0][0]
+                # matrices can be multiplied
+                # return self.values[0][0] * other.values[0][0]
+                for idx_row_m1, row_m1 in enumerate(self.values):  # for each row in matrix1
+                    # for idx_col_m1 in range(0, len(self.values[0])):  # for each col in matrix1
+                    # print(idx_row_m1, row_m1)
+                    product_row = []
+                    for idx_col_m2 in range(0, len(other.values[0])):  # for each col in matrix2
+                        col_m2 = []
+                        for row_m2 in other.values:  # for each row in matrix2
+                            col_m2.append(row_m2[idx_col_m2])
+                        # multiply row_m1 and col_m2
+                        product = 0
+                        for i in range(0, len(row_m1)):
+                            product += row_m1[i] * col_m2[i]
+                        product_row.append(product)
+                    product_matrix.append(product_row)
+                return Matrix(product_matrix)
             else:
                 raise ValueError("<Matrix multiplication criteria not met>")
 
@@ -36,7 +52,9 @@ def main():
     mat1 = Matrix(A)
     mat2 = Matrix(B)
     mat3 = mat1 @ mat2
-    print(mat3)
+    print(mat3.values)
+    mat3 = mat2 @ mat1
+    print(mat3.values)
 
 
 if __name__ == "__main__":
