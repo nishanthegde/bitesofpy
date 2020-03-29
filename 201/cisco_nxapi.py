ba6557bf8895ba9640ae0e7ee4d8e94511320321
nishant hegde <nhegde@netflix.com>
2020-03-28 20:48:20 -0700

    kata submission 0328

--- 201/cisco_nxapi.py
+++ 201/cisco_nxapi.py
@@ -13,7 +13,8 @@ def nxapi_show_version():
                            "version": 1}, "id": 1}]
     # 1. use requests to post to the switch
     response = ...
-
+    print(type(response))
+    print(response)
     # 2. retrieve and return the kickstart_ver_str from the response
     # example response json:
     # {'result': {'body': {'bios_cmpl_time': '05/17/2018',
@@ -30,4 +31,5 @@ def nxapi_show_version():
 if __name__ == '__main__':
     print('please let everyone be safe...')
     result = nxapi_show_version()
+    print(type(result))
     print(result)
