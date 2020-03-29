bbc4ef9991278026354d8f3176f4fadf9623dedd
nishant hegde <nhegde@netflix.com>
2020-03-29 12:25:34 -0700

    kata submission 0329

--- 201/cisco_nxapi.py
+++ 201/cisco_nxapi.py
@@ -3,18 +3,24 @@ import requests
 
 def nxapi_show_version():
     url = """ please fill in """
-    switchuser = """ please fill in """
-    switchpassword = """ please fill in """
+    switchuser = 'admin'
+    switchpassword = 'Admin_1234!'
+
+    url='http://<IP_Address>/ins'
+    switchuser='<User_ID>'
+    switchpassword='<Password>'
+
 
     http_headers = {""" please fill in """}
     payload = [{"jsonrpc": "2.0",
-                "method": """ please fill in """,
-                "params": {"cmd": """ please fill in """,
+                "method": 'cli',
+                "params": {"cmd": 'cli_show',
                            "version": 1}, "id": 1}]
     # 1. use requests to post to the switch
     response = ...
     print(type(response))
     print(response)
+
     # 2. retrieve and return the kickstart_ver_str from the response
     # example response json:
     # {'result': {'body': {'bios_cmpl_time': '05/17/2018',
