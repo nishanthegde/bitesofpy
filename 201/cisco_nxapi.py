38783cfa518437321e94a62cc7a00501d12aa99e
nishant hegde <nhegde@netflix.com>
2020-03-31 11:10:50 -0700

    kata submission 0331a

--- 201/cisco_nxapi.py
+++ 201/cisco_nxapi.py
@@ -8,16 +8,7 @@ def nxapi_show_version():
     switchpassword = 'Admin_1234!'
 
     http_headers = {'content-type': 'application/json-rpc'}
-    # payload = [{
-    #     "ins_api": {
-    #         "version": "1.0",
-    #         "type": "cli_show",
-    #         "chunk": "0",
-    #         "sid": "1",
-    #         "input": "show version",
-    #         "output_format": "json"
-    #     }
-    # }]
+
     payload = [
         {
             "jsonrpc": "2.0",
@@ -51,11 +42,11 @@ def nxapi_show_version():
     #             }
     # }
 
-    version = ...
+    # version = ...
 
-    if response.status_code == 200:
-        #     # print(response.json())
-        version = response.json()['result']['body']['kickstart_ver_str']
+    # if response.status_code == 200:
+    #     #     # print(response.json())
+    version = response.json()['result']['body']['kickstart_ver_str']
 
     return version
 
