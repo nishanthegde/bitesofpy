51331eb950743132822e8142bdc3b3ba1ff24f48
nishant hegde <nhegde@netflix.com>
2020-04-05 14:31:48 -0700

    kata submission 0405

--- 204/pomodoro.py
+++ 204/pomodoro.py
@@ -1,5 +1,7 @@
 import time
 from typing import Union
+import asyncio
+
 # ONE_MIN = 60
 # FIVE_MIN = ONE_MIN * 5
 # TWENTY_FIVE_MIN = ONE_MIN * 25
@@ -24,20 +26,22 @@ async def break_time(delay: Union[float, int], loop: int) -> None:
     """
     _delay = int(delay / ONE_MIN)
     print(f"[{loop}] {time.strftime('%X')} Time for a {_delay} min break!")
-    time.sleep(delay)
+    # time.sleep(delay)
+    await asyncio.sleep(delay)
 
 
-def lunch_time(delay):
+async def lunch_time(delay: Union[float, int]) -> None:
     """Lunch time
 
     :param delay: float of delay in seconds
     :return: None
     """
     print(f"\n** {time.strftime('%X')} Time for lunch! **")
-    time.sleep(delay)
+    # time.sleep(delay)
+    await asyncio.sleep(delay)
 
 
-def work_time(delay, loop):
+async def work_time(delay: Union[float, int], loop: int) -> None:
     """Work time
 
     :param delay: float of delay in seconds
@@ -45,14 +49,15 @@ def work_time(delay, loop):
     :return: None
     """
     print(f"[{loop}] {time.strftime('%X')} Time to work!")
-    time.sleep(delay)
+    # time.sleep(delay)
+    await asyncio.sleep(delay)
 
 
-def session(
-    work_length=TWENTY_FIVE_MIN,
-    short_break_length=FIVE_MIN,
-    long_break_length=THIRTY_MIN,
-):
+async def session(
+    work_length: Union[int, float]=TWENTY_FIVE_MIN,
+    short_break_length: Union[int, float]=FIVE_MIN,
+    long_break_length: Union[int, float]=THIRTY_MIN,
+) -> None:
     """Session
 
     :param work_length: float of work length in seconds
@@ -63,22 +68,22 @@ def session(
     loop = 1
 
     while loop < 4:
-        work_time(work_length, loop)
-        break_time(short_break_length, loop)
+        await work_time(work_length, loop)
+        await break_time(short_break_length, loop)
         loop += 1
 
-    work_time(work_length, loop)
+    await work_time(work_length, loop)
 
     if CURRENT_SESSION % 2 != 0:
-        break_time(long_break_length, loop)
+        await break_time(long_break_length, loop)
 
 
-def main(
-    work_length=TWENTY_FIVE_MIN,
-    short_break_length=FIVE_MIN,
-    long_break_length=THIRTY_MIN,
-    lunch_length=HOUR,
-):
+async def main(
+    work_length: Union[int, float]=TWENTY_FIVE_MIN,
+    short_break_length: Union[int, float]=FIVE_MIN,
+    long_break_length: Union[int, float]=THIRTY_MIN,
+    lunch_length: Union[int, float]=HOUR,
+) -> None:
     """Main entry point
 
     :param work_length: float of work length in seconds
@@ -87,16 +92,16 @@ def main(
     :param lunch_length: float of lunch length in seconds
     :return: None
     """
-    print('set focus on code session finnish kata')
+    # print('set focus on code session finnish kata')
     global CURRENT_SESSION
     # print(CURRENT_SESSION)
     print(f"Pomodor timer started at: {time.strftime('%X')}")
 
     while CURRENT_SESSION <= 4:
         print(f"\nSession: {CURRENT_SESSION}")
-        session(work_length, short_break_length, long_break_length)
+        await session(work_length, short_break_length, long_break_length)
         if CURRENT_SESSION == 2:
-            lunch_time(lunch_length)
+            await lunch_time(lunch_length)
         CURRENT_SESSION += 1
 
     print(f"\n{time.strftime('%X')} Time to go home!")
@@ -105,4 +110,5 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    # asyncio.run(main(0.0025, 0.0005, 0.003, 0.01))
+    asyncio.run(main())
