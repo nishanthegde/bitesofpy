4974ddde6bd048220c54dd8f19d0dced4b7ee460
nishant hegde <nhegde@netflix.com>
2020-04-07 17:56:51 -0700

    kata submission 0407

--- 63/traffic.py
+++ 63/traffic.py
@@ -9,11 +9,14 @@ def traffic_light():
     """Returns an itertools.cycle iterator that
        when iterated over returns State namedtuples
        as shown in the Bite's description"""
-    pass
+    return cycle([-1, 1])
 
 
 if __name__ == '__main__':
+    print('thank you for everything...')
     # print a sample of 10 states if run as standalone program
-    for state in islice(traffic_light(), 10):
-        print(f'{state.command}! The light is {state.color}')
-        sleep(state.timeout)
+    # for state in islice(traffic_light(), 10):
+    #     print(f'{state.command}! The light is {state.color}')
+    #     sleep(state.timeout)
+    it = traffic_light()
+    print(len(list(islice(it, 100, 217))))
