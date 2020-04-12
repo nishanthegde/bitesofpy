7fcb20d2942eb3163ca355d4c7b172afa227110a
nishant hegde <nhegde@netflix.com>
2020-04-12 15:56:02 -0700

    kata submission 0412

--- 88/monitor.py
+++ 88/monitor.py
@@ -9,20 +9,38 @@ ALERT_MSG = 'ALERT: suffering performance hit today'
 
 violations = Counter()
 
+
 def get_today():
     """Making it easier to test/mock"""
     return date.today()
 
 
 @contextmanager
-def timeit():
-    pass
+def timeit() -> None:
+    start = time()
+    yield
+    elapsed_time = time() - start
+
+    if elapsed_time >= OPERATION_THRESHOLD_IN_SECONDS:
+        violations.update({get_today().strftime('%s')})
+
+    if violations[get_today().strftime('%s')] >= ALERT_THRESHOLD:
+        # print('ellapsed_time time: {} seconds'.format(elapsed_time))
+        print(ALERT_MSG)
 
 
 def main():
-    print('i am grateful for all that you have given me... ')
-    print('kata will be finished tomorrow')
-    print(type(violations))
+
+    print('thank you for everything that you have given me')
+
+    with timeit():
+        s = [x for x in range(1000000)]
+    with timeit():
+        s = [x for x in range(100000)]
+    with timeit():
+        s = [x for x in range(100000)]
+
+    # print(violations[get_today().strftime('%s')])
 
 
 if __name__ == '__main__':
