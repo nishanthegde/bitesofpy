62a6c34916bf24f799c4fb3b9184b6bbe2a9e3a3
nishant hegde <nhegde@netflix.com>
2020-04-13 10:57:56 -0700

    kata submission 0413

--- 116/files.py
+++ 116/files.py
@@ -5,4 +5,21 @@ ONE_KB = 1024
 
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
-    pass
+    f_list = []
+    for path, dirs, files in os.walk(dirname):
+        for f in files:
+            size_bytes = os.path.getsize(os.path.join(path, f))
+            print(size_bytes / ONE_KB)
+            if size_bytes / ONE_KB >= size_in_kb:
+                f_list.append(f)
+            # print(path, f, )
+    return f_list
+
+
+def main():
+    print('please look after everyone...')
+    print([f for f in get_files('../115', .35)])
+
+
+if __name__ == '__main__':
+    main()
