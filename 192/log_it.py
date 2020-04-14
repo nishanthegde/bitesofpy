d83034a5be948becaf8997aeaf29cddd719625cc
nishant hegde <nhegde@netflix.com>
2020-04-14 14:54:18 -0700

    kata submission 0414

--- 192/log_it.py
+++ 192/log_it.py
@@ -1,38 +1,59 @@
 import logging
 from typing import Callable
-# import pytest
-# import test_log_it
 
 
-DEBUG = ""logger.setLevel(logging.DEBUG)""
-INFO = logger.setLevel(logging.INFO)
-WARNING = logger.setLevel(logging.WARNING)
-ERROR = logger.setLevel(logging.ERROR)
-CRITICAL = logger.setLevel(logging.CRITICAL)
+def set_debug_level():
+    logging.getLogger().setLevel("DEBUG")
+
+
+def set_info_level():
+    logging.getLogger().setLevel("INFO")
+
+
+def set_warning_level():
+    logging.getLogger().setLevel("WARNING")
+
+
+def set_error_level():
+    logging.getLogger().setLevel("ERROR")
+
+
+def set_critical_level():
+    logging.getLogger().setLevel("CRITICAL")
+
+
+DEBUG = set_debug_level
+INFO = set_info_level
+WARNING = set_warning_level
+ERROR = set_error_level
+CRITICAL = set_critical_level
 
 
 def log_it(level: Callable, msg: str) -> None:
 
-    logger.debug(msg)
-    # if level == 'debug':
+    logger = logging.getLogger('pybites_logger')
+
+    if level == DEBUG:
+        logger.debug(msg)
+    elif level == INFO:
+        logger.info(msg)
+    elif level == WARNING:
+        logger.warning(msg)
+    elif level == ERROR:
+        logger.error(msg)
+    elif level == CRITICAL:
+        logger.critical(msg)
 
 
 def main():
-    print('thank you for everything ...')
-
-    # for level in test_log_it.LOG_LEVEL:
-    #     print(type(test_log_it.LOG_LEVEL[level]))
-    # assert callable(test_log_it.LOG_LEVEL[level])
-    # assert callable(DEBUG)
-    # print(type(DEBUG))
-    # log_it("debug", "This is a debug message")
-    # caplog.set_level(logging.DEBUG)
-
-    logger = logging.getLogger('pybites_logger')
-    logging.basicConfig(format='%(name)s-%(levelname)s-%(message)s')
-    logger.setLevel(logging.DEBUG)
-    print(type(logger.error('This is a debug message')))
+    print('thank you for everything... ')
+    print(callable(DEBUG))
 
 
 if __name__ == "__main__":
     main()
+    log_it(DEBUG, "This is a debug message.")
+    # log_it(INFO, "This is an info message.")
+    # log_it(WARNING, "This is a warning message.")
+    # log_it(ERROR, "This is an error message.")
+    # log_it(CRITICAL, "This is a critical message.")
