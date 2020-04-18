3b2ea13c032e190eb87be7b3c6e09fe7e6730a35
nishant hegde <nhegde@netflix.com>
2020-04-17 19:14:52 -0700

    kata submissioon 0417

--- 243/test_ips.py
+++ 243/test_ips.py
@@ -11,8 +11,8 @@ from ips import (ServiceIPRange, parse_ipv4_service_ranges,
 
 URL = "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
 
-# local = os.getcwd()
-local = '/tmp'
+local = os.getcwd()
+# local = '/tmp'
 # TMP = os.getenv("TMP", "/tmp")
 
 TMP = os.getenv("TMP", local)
@@ -30,48 +30,56 @@ def json_file():
     return PATH
 
 
+def test_json(json_file):
+    assert str(json_file) != ''
+
+
 def test_service_ips(json_file):
-    source_path = json_file
-    ranges = parse_ipv4_service_ranges(source_path)
+    ranges = parse_ipv4_service_ranges(json_file)
     assert len(ranges) == 1886
     assert type(ranges) == list
     assert all(isinstance(x, ServiceIPRange) for x in ranges)
 
 
 def test_aws_service_range_with_valid_ip(json_file):
-    source_path = json_file
-    ranges = parse_ipv4_service_ranges(source_path)
+    ranges = parse_ipv4_service_ranges(json_file)
     address = '54.244.46.0'
     aws_service_ranges = get_aws_service_range(address, ranges)
     assert len(aws_service_ranges) == 3
-    # c = [r.cidr for r in aws_service_ranges]
-    # assert IPv4Network('54.244.0.0/16') in c
-    # assert IP not in c
 
 
 def test_aws_service_range_with_invalid_ip(json_file):
     with pytest.raises(ValueError):
-        source_path = json_file
-        ranges = parse_ipv4_service_ranges(source_path)
+        ranges = parse_ipv4_service_ranges(json_file)
         address = '366.1.2.2'
         assert get_aws_service_range(address, ranges)
 
 
-def test_no_address(json_file):
-    source_path = json_file
-    ranges = parse_ipv4_service_ranges(source_path)
+def test_aws_service_range_with_no_address(json_file):
+    ranges = parse_ipv4_service_ranges(json_file)
     address = '192.0.2.8'
     aws_service_ranges = get_aws_service_range(address, ranges)
     assert not aws_service_ranges
 
 
-# def test_val1(json_file):
-#     source_path = json_file
-#     ranges = parse_ipv4_service_ranges(source_path)
-#     with pytest.raises(ValueError) as exc:
-#         address = -10
-#         get_aws_service_range(address, ranges)
-#         assert 'Address must be a valid IPv4 address' in str(exc)
+def test_aws_service_range_with_invalid_ip_message(json_file):
+    ranges = parse_ipv4_service_ranges(json_file)
+    with pytest.raises(ValueError) as exc:
+        address = -10
+        get_aws_service_range(address, ranges)
+        assert 'Address must be a valid IPv4 address' in str(exc)
+
+
+def test_out(json_file, capsys):
+    ranges = parse_ipv4_service_ranges(json_file)
+    address = '54.244.46.0'
+    aws_service_ranges = get_aws_service_range(address, ranges)
+
+    assert aws_service_ranges[0]
+
+    print(aws_service_ranges[0])
+    captured = capsys.readouterr()
+    assert captured.out == "54.244.0.0/16 is allocated to the AMAZON service in the us-west-2 region\n"
 
 
 # def test_val2(json_file):
@@ -98,14 +106,3 @@ def test_no_address(json_file):
 #     address = '192.0.2.8'
 #     aws_service_ranges = get_aws_service_range(address, ranges)
 #     assert len(aws_service_ranges) == 0
-
-
-# def test_out(json_file, capsys):
-#     source_path = json_file
-#     ranges = parse_ipv4_service_ranges(source_path)
-#     address = '54.244.46.0'
-#     aws_service_ranges = get_aws_service_range(address, ranges)
-
-#     print(aws_service_ranges[0])
-#     captured = capsys.readouterr()
-#     assert captured.out == "54.244.0.0/16 is allocated to the AMAZON service in the us-west-2 region\n"
