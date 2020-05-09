a74124095ba1b180c32ba09304ffbc3238ffcc69
nishant hegde <nhegde@netflix.com>
2020-05-09 13:51:10 -0700

    kata submission 05/09

--- 271/cls.py
+++ 271/cls.py
@@ -3,4 +3,4 @@ import inspect
 
 def get_classes(mod):
     """Return a list of all classes in module 'mod'"""
-    pass
+    return [name for name, obj in inspect.getmembers(mod) if inspect.isclass(obj) and name[0].isupper()]
