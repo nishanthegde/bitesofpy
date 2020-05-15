3ca9538cbc8a2f66b9f1b8a28b7acaaf6100ae5f
nishant hegde <nhegde@netflix.com>
2020-05-14 20:57:25 -0700

    kata submission 0514

--- 247/test_color.py
+++ 247/test_color.py
@@ -1,7 +1,6 @@
 from unittest.mock import patch, Mock
 
 import pytest
-
 import color
 
 
@@ -14,11 +13,14 @@ def test_gen_hex_color(gen):
     assert isinstance(next(gen), str)
 
 
-def main():
-    print('thank you for everything 4/29...')
-    print(gen)
-    print(next(color.gen_hex_color()))
+def test_gen_random_called(gen):
+    with patch('color.sample', return_value=(108, 144, 121)) as mock_random:
+        assert next(gen) == '#6C9079'
+
+# def main():
+#     print(next(gen))
+#     print(next(color.gen_hex_color()))
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
