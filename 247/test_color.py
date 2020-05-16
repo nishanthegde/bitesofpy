5ebe80662415d6c32f847d05c004deedb968a57e
nishant hegde <nhegde@netflix.com>
2020-05-15 20:56:42 -0700

    515

--- 247/test_color.py
+++ 247/test_color.py
@@ -17,6 +17,10 @@ def test_gen_hex_color_len(gen):
     assert len(next(gen)) == 7
 
 
+def test_gen_hex_color_range(gen):
+    assert '100' not in next(gen)
+
+
 def test_gen_random_called(gen):
     with patch('color.sample', return_value=(0, 144, 255)) as mock_random:
         assert next(gen) == '#0090FF'
