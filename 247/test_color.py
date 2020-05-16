ae0de9b38ee87e92879ff5d799b152d1f479ac40
nishant hegde <nhegde@netflix.com>
2020-05-15 20:21:42 -0700

    kata submit 515

--- 247/test_color.py
+++ 247/test_color.py
@@ -13,13 +13,17 @@ def test_gen_hex_color(gen):
     assert isinstance(next(gen), str)
 
 
+def test_gen_hex_color_len(gen):
+    assert len(next(gen)) == 7
+
+
 def test_gen_random_called(gen):
     with patch('color.sample', return_value=(0, 144, 255)) as mock_random:
         assert next(gen) == '#0090FF'
 
 
 # def test_gen_random_called2(gen):
-#     with patch('color.sample', return_value=(0, 20, 19)) as mock_random2:
+#     with patch('color.sample', return_value=(1, 256, 19)) as mock_random:
 #         assert next(gen) == '#001413'
 
 
