96b67eb87200f35ffa2ea240fb10622b6167aa6e
nishant hegde <nhegde@netflix.com>
2020-05-21 15:50:48 -0700

    kata submission 521

--- 268/ops.py
+++ 268/ops.py
@@ -1,4 +1,39 @@
-def num_ops(n):
+# import math
+
+# def _is_power_of_2(n: int) -> bool:
+
+#     x = 1
+
+#     if n == 1:
+#         return True
+#     else:
+#         while math.pow(2, x) <= n:
+#             if math.pow(2, x) == n:
+#                 return True
+#             x += 1
+#     return False
+
+import queue
+from collections import deque
+
+class Node:
+
+    def __init__(self, val, level):
+        self.val = val
+        self.level = level
+
+
+class ImprovedQueue(queue.Queue):
+
+    def to_list(self):
+        """
+        Returns a copy of all items in the queue without removing them.
+        """
+        with self.mutex:
+            return list(self.queue)
+
+
+def num_ops(n: int) -> int:
     """
     Input: an integer number, the target number
     Output: the minimum number of operations required to reach to n from 1.
@@ -14,4 +49,93 @@ def num_ops(n):
     """
     # you code
 
-    return ops_num
+    # base = 1  # base
+    # ops = 0  # number of ops
+
+    # result = base * 2
+    # # print(result)
+    # ops += 1
+    # i = 1
+    # # while result != n and i < 20:
+    # while result != n:
+    #     if result < n:
+    #         result *= 2
+    #         ops += 1
+    #         print(result, ops, 'x')
+    #     if result > n:
+    #         result = result // 3
+    #         ops += 1
+    #         print(result, ops, '//')
+    #     i += 1
+
+    # return ops
+
+    # To keep track of visited numbers in BFS
+    visit = set()
+
+    # Create a queue and enqueue base into it
+    # q = ImprovedQueue()
+    q = deque()
+    x = Node(1, 0)
+    # q.put(x)
+    q.append(x)
+
+    loop_num = 1
+
+    # Do BFS starting from x
+    # while (not q.empty()):
+    while q:
+
+        # print(q.qsize())
+        # Remove an item from queue
+        # t = q.get()
+        t = q.popleft()
+        print(t.val)
+        # If the removed item is target number y, return its level
+        if (t.val == n):
+            return t.level
+
+        # Mark dequeued number as visited
+        visit.add(t.val)
+        # print(visit, loop_num)
+
+        # If we can reach n in one more step
+        if (t.val * 2 == n or t.val // 3 == n):
+            return t.level + 1
+
+        # Insert children of t if not visited already
+        if (t.val * 2 not in visit):
+            # print('here')
+            x.val = t.val * 2
+            x.level = t.level + 1
+            # print(x.val, x.level)
+            # q.put(x)
+            # print(x.val, x.level)
+            # q.append(x)
+            q.append(Node(t.val * 2, t.level + 1))
+            print([e.val for e in q])
+            # print([elem.val for elem in list(q)])
+        if (t.val // 3 > 0 and t.val // 3 not in visit):
+            # print('here2')
+            x.val = t.val // 3
+            x.level = t.level + 1
+            # print(x.val, x.level)
+            # q.put(x)
+            # print(x.val, x.level)
+            # q.append(x)
+            q.append(Node(t.val // 3, t.level + 1))
+            print([e.val for e in q])
+            # print([elem.val for elem in list(q)])
+        loop_num += 1
+
+
+def main():
+    print('please look after west bengal...')
+    print(num_ops(55))
+    # n1 = Node(1, 0)
+    # print(n1.val, n1.level)
+    # print(type(n1))
+
+
+if __name__ == '__main__':
+    main()
