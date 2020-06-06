f29daf4c06f94ab6b94481615c1014cc8e41096e
nishant hegde <nhegde@netflix.com>
2020-06-06 13:28:20 -0700

    kata submitted 06062020

--- 267/island.py
+++ 267/island.py
@@ -1,28 +1,50 @@
+from typing import List
+
 # Hint:
-# You can define a helper funtion: get_others(map, row, col) to assist you.
+# You can define a helper function: get_others(map, row, col) to assist you.
 # Then in the main island_size function just call it when traversing the map.
 
-
-def get_others(map_, r, c):
+def get_others(map_: List, r: int, c: int) -> int:
     """Go through the map and check the size of the island
        (= summing up all the 1s that are part of the island)
 
        Input - the map, row, column position
-       Output - return the total numbe)
+       Output - return the total number)
     """
     nums = 0
-    # your code here
+    over = 0
 
-    return nums
+    if map_[r][c] == 1:
+        nums += 1
+        if c > 0 and map_[r][c - 1] == 1:
+            over += 2
+        if r > 0 and map_[r-1][c] == 1:
+            over += 2
+
+    # print((nums * 4) - over)
+    return (nums * 4) - over
 
 
-def island_size(map_):
+def island_size(map_: List) -> int:
     """Hint: use the get_others helper
 
     Input: the map
     Output: the perimeter of the island
     """
     perimeter = 0
-    # your code here
+    rows = len(map_)
+    cols = len(map_[0])
+
+    for i in range(0, rows):
+        for j in range(0, cols):
+            perimeter += get_others(map_, i, j)
 
     return perimeter
+
+
+def main():
+    print(island_size(rectangle))
+
+
+if __name__ == '__main__':
+    main()
