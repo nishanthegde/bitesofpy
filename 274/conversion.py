73a0b91114b89b19150eec09a06fd912c13b0f4c
nishant hegde <nhegde@netflix.com>
2020-06-23 10:55:22 -0700

    kata submission 623

--- 274/conversion.py
+++ 274/conversion.py
@@ -4,6 +4,20 @@ def dec_to_base(number, base):
            base is the new base  (eg. 2, 6, or 8)
     Output: the converted number in the new base without the prefix (eg. '0b')
     """
-    # your code
+    add = number % base
 
-    # return n
+    if number <= 1:
+        if number == 0:
+            return ''
+        return str(number)
+
+    else:
+        return int(str(dec_to_base(number // base, base)) + str(add))
+
+
+def main():
+    print(dec_to_base(256, 8))
+
+
+if __name__ == "__main__":
+    main()
