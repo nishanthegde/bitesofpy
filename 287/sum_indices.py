a8d7e3a5d6c0f637d7558dbcf94a784675e34b6e
nishant hegde <nhegde@netflix.com>
2020-07-17 19:24:40 -0700

    kata submission 0717

--- 287/sum_indices.py
+++ 287/sum_indices.py
@@ -1,5 +1,20 @@
 from typing import List
 
 
+def get_indices(items: List[str], element: str) -> List[int]:
+    indices = []
+
+    for i in range(len(items)):
+        if element == items[i]:
+            indices.append(i)
+
+    return indices
+
+
 def sum_indices(items: List[str]) -> int:
-    ...
+    tot_ind = 0
+
+    for i in range(len(items)):
+        tot_ind += sum([k for k in get_indices(items, items[i]) if k <= i])
+
+    return tot_ind
