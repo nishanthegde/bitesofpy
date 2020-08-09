cb74893d113d56cb4f3008da956afa3642ac20f1
nishant hegde <nhegde@netflix.com>
2020-08-08 17:14:04 -0700

    kata submission 0808

--- 284/pascal.py
+++ 284/pascal.py
@@ -2,8 +2,15 @@ from typing import List
 
 
 def pascal(N: int) -> List[int]:
-    """
-    Return the Nth row of Pascal triangle
-    """
-    # you code ...
-    # return row
+    start = [1]
+    if N == 0:
+        return []
+
+    for i in range(N - 1):
+        start = [0] + start + [0]
+        res = []
+        for j in range(len(start) - 1):
+            res.append(start[j] + start[j + 1])
+        start = res
+
+    return start
