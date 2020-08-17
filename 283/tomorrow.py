d5b0f27192f6ec68e8924f05bdb6c02a52cf4a49
nishant hegde <nhegde@netflix.com>
2020-08-17 10:27:07 -0700

    kata submission 817

--- 283/tomorrow.py
+++ 283/tomorrow.py
@@ -2,5 +2,10 @@ import datetime
 from datetime import date
 
 
-def tomorrow(today=date.today()):
-    return today + datetime.timedelta(days=1)
+def tomorrow(*args):
+    if not args:
+        return date.today() + datetime.timedelta(days=1)
+    else:
+        for x in args:
+            return x + datetime.timedelta(days=1)
+
