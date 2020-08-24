f0dd3a7d11af85f9b0ec878ac348298d05c79499
nishant hegde <nhegde@netflix.com>
2020-08-24 13:33:01 -0700

    kata submission 824

--- 294/bowling_game.py
+++ 294/bowling_game.py
@@ -59,38 +59,36 @@ def calculate_score(frames: str) -> int:
     for k, v in score.items():
         # if bonus is spare
         if v[3] == 1:
-            # update score with roll 1 score from next frame do this even for 10th frame
-            v[2] += score[k + 1][0]
+            # not last frame
+            if k < 11:
+                # update score with roll 1 score from next frame do this even for 10th frame
+                v[2] += score[k + 1][0]
+            else:
+                # last frame
+                v[2] = 10 - score[k - 1][1]
 
         # if bonus is strike
         if v[3] == 2 and k <= 9:
             # first update score with roll 1 score from next frame 10th frame does not get updated
             v[2] += score[k + 1][0]
-            # if the next frame is not a strike
-            if score[k + 1][3] != 2:
-                # then update score with roll 2 score from next frame
+            # if the next frame has a roll 2 score or the next frame is 10
+            if score[k + 1][1] > 0 or k + 1 == 10:
                 v[2] += score[k + 1][1]
             else:
-                # if the next frame is also a strike then get roll 1 score from the frame after next
-                v[2] += score[k + 2][0]
+                # next frame does not have a roll 2 score either because a miss or strike
+                # if the next frame is a strike
+                if score[k + 1][3] == 2:
+                    # then update score with roll 1 score from next frame
+                    v[2] += score[k + 2][0]
+                # else:
+                #     # if the next frame is also a strike then get roll 1 score from the frame after next
+                #     v[2] += score[k + 2][0]
 
     final = sum(score[frame][2] for frame in score if frame <= 10)
 
     # add last frame if 10th frame has a strike
     if score[10][3] == 2:
-        final += score[11][0]
+        final += score[11][2]
 
+    # print(score)
     return final
-
-
-def main():
-    print('thank you for the waves this morning and thank you for looking after my mama...')
-    print(calculate_score('--------------------'))
-    print(calculate_score('8/9-X X 6/4/X 8-X XXX'))
-    print(calculate_score('8/549-X X 5/53639/9/X'))
-    print(calculate_score('36546/819/7--/717/3/-'))
-    print(calculate_score('X X X X X X X X 54X54'))
-
-
-if __name__ == "__main__":
-    main()
