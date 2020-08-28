566cfe10dea6f535a3e4a48fee607358a8af286e
nishant hegde <nhegde@netflix.com>
2020-08-27 18:22:17 -0700

    kata submission 827

--- 280/regex_lookahead_lookbehind.py
+++ 280/regex_lookahead_lookbehind.py
@@ -2,22 +2,18 @@ import re
 
 
 def count_n_repetitions(text, n=1):
-    """
-    Counts how often characters are followed by themselves for
-    n times.
-
-    text: UTF-8 compliant input text
-    n: How often character should be repeated, defaults to 1
-    """
     ret = 0
-    regex = r'((\w)\2{' + re.escape(str(n)) + ',})'
+    regex = r'((.)\2{' + re.escape(str(n)) + ',})'
 
+    text = re.sub(r"\n", " ", text)
     match = re.findall(regex, text)
+
     # print(match)
+
     ret = len(match)
     if match:
         for m in match:
-            ret += count_n_repetitions(m[0][1:],n)
+            ret += count_n_repetitions(m[0][1:], n)
 
     return ret
 
@@ -31,6 +27,19 @@ def count_n_reps_or_n_chars_following(text, n=1, char=""):
     n: How often character should be repeated, defaults to 1
     char: Character which also counts if repeated n times
     """
+
+    # first count n repetitions
+    ret = count_n_repetitions(text, n)
+
+    if char:
+        # print(char)
+        regex = r'(.)' + re.escape(char) + '{' + re.escape(str(n)) + '}'
+        match = re.findall(regex, text)
+        # print(regex)
+        if match:
+            ret += len(match)
+
+    return ret
 
 
 def check_surrounding_chars(text, surrounding_chars):
@@ -47,11 +56,12 @@ def main():
     print('please look after my mama. i love her very much...')
     # print(count_n_repetitions('3444553333', 2))
     # print(count_n_repetitions('333', 1))
-    print(count_n_repetitions('', 1))
-    print(count_n_repetitions('111', 2))
-    print(count_n_repetitions('1122', 2))
-    print(count_n_repetitions('1112345', 2))
+    # print(count_n_repetitions('', 1))
+    # print(count_n_repetitions('111', 2))
+    # print(count_n_repetitions('ßßß', 1))
+    # print(count_n_repetitions("\n\n\nAs are newlines\n\n\n", 2))
 
+    print(count_n_reps_or_n_chars_following('4455???', n=2, char='?'))
 
 
 if __name__ == '__main__':
