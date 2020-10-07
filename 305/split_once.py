209cf395d4f602b43d795a993234594f351e6885
nishant hegde <nhegde@netflix.com>
2020-10-07 16:42:08 -0700

    kata submission 1007

--- 305/split_once.py
+++ 305/split_once.py
@@ -1,12 +1,13 @@
 from typing import List
 import re
-
+from textwrap import dedent
 
 def split_once(text: str, separators: str = None) -> List[str]:
     return_value = list()
 
     if not separators:
         ws = ' \t\n\v\f\r'
+
         for s in ws:
             if len(text.split(s, 1)) > 1:
                 if len(return_value) > 1:
@@ -14,23 +15,22 @@ def split_once(text: str, separators: str = None) -> List[str]:
                 return_value.append(text.split(s, 1)[0])
                 return_value.append(text.split(s, 1)[1])
                 text = text.split(s, 1)[1]
+    else:
+
+        for s in separators:
+            if len(text.split(s, 1)) > 1:
+                if not return_value:
+                    return_value.append(text.split(s, 1)[0])
+                    return_value.append(text.split(s, 1)[1])
+                    # print(return_value)
+                else:
+                    for i, t in enumerate(return_value):
+                        if len(t.split(s, 1)) > 1:
+                            return_value[i] = t.split(s, 1)[0]
+                            return_value.insert(i+1, t.split(s, 1)[1])
+                            break
 
     if not return_value:
         return_value.append(text)
 
     return return_value
-
-
-def main():
-    print("please look after my mama...")
-    print(split_once(''))
-    print(split_once('abc'))
-    print(split_once('abc def'))
-    print(split_once('abc\tdef'))
-    print(split_once('abc def ghi'))
-    print(split_once('abc def\tghi'))
-    print(split_once('abc def\tghi jkl\tmno'))
-
-
-if __name__ == "__main__":
-    main()
