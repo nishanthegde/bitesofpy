407279b0f7ae90b63131bc740674c50592f90185
nishant hegde <nhegde@netflix.com>
2020-10-10 12:31:50 -0700

    kata submission 1010

--- 206/check_split.py
+++ 206/check_split.py
@@ -19,34 +19,19 @@ def check_split(item_total, tax_rate, tip, people):
     tax_rate_float = Decimal(tax_rate.strip('%'))
     tip_float = Decimal(tip.strip('%'))
 
+    tax = (item_total_float * (tax_rate_float / 100)).quantize(TWOPLACES)
+    total = (item_total_float + tax).quantize(TWOPLACES)
+    tip = (total * (tip_float / 100)).quantize(TWOPLACES)
 
+    grand_total = (total + tip).quantize(TWOPLACES)
 
-    total = (item_total_float * (1 + (tax_rate_float / 100))).quantize(TWOPLACES)
-    grand_total = (total * (1 + (tip_float / 100))).quantize(TWOPLACES)
+    splits = list()
 
-    # total1 = item_total_float * (1 + (tax_rate_float / 100))
-    # grand_total1 = total1 * (1 + (tip_float / 100))
+    for i in range(0, people):
+            splits.append((grand_total / people).quantize(TWOPLACES))
 
-    s = [(grand_total / people) for i in range(0, people)]
+    remaining = grand_total - sum(splits)
+    splits[-1] += remaining
 
-    return f'${str(grand_total)}', s, sum(s)
+    return f'${str(grand_total)}', splits
 
-
-def main():
-    print("thank you for looking after my family...")
-    print(check_split('$8.68', '4.75%', '10%', 3))
-    print(check_split('$8.44', '6.75%', '11%', 3))
-    print(check_split('$9.99', '3.25%', '10%', 2))
-    print(check_split('$186.70', '6.75%', '18%', 6))
-    print(check_split('$191.57', '6.75%', '15%', 6))
-    print(check_split('$0.00', '0%', '0%', 1))
-    print(check_split('$100.03', '0%', '0%', 4))
-    print(check_split('$141.86', '2%', '18%', 9))
-    print(check_split('$16.99', '10%', '20%', 1))
-    print(check_split('$16.99', '10%', '20%', 2))
-    print(check_split('$16.99', '10%', '20%', 3))
-    print(check_split('$16.99', '10%', '20%', 4))
-
-
-if __name__ == "__main__":
-    main()
