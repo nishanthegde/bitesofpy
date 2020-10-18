2528b90d0ab04abd905dae2ce0ca2b9a9cab9ff3
nishant hegde <nhegde@netflix.com>
2020-10-18 14:20:06 -0700

    kata submission 1018

--- 303/unique_genes.py
+++ 303/unique_genes.py
@@ -28,6 +28,8 @@ def write_test_file(filename, content, zip=False):
                 f.write(make_fasta_from_tuple(content))
 
 
+
+
 def convert_to_unique_genes(filename_in, filename_out):
     """
     Takes a standard FASTA file or gzipped FASTA file,
@@ -43,13 +45,27 @@ def convert_to_unique_genes(filename_in, filename_out):
 
     with open(filename_in, "r") as f:
         all_lines = f.readlines()
+    # print(all_lines)
 
-    for i in range(len(all_lines) - 2, -1, -1):
-        if all_lines[i][0:5].strip().replace('>', '').lower() != 'gene' and all_lines[i + 1][0:5].strip().replace('>',
-                                                                                                                  '').lower() != 'gene':
-            all_lines[i] = all_lines[i].strip() + all_lines.pop(i + 1).strip()
-            # print(all_lines[i].strip()+all_lines[i+1].strip())
+    if all_lines[0][0:5].strip().replace('>', '').lower() == 'gene':
 
+        for i in range(len(all_lines) - 2, -1, -1):
+            curr_gene = all_lines[i][0:5].strip().replace('>', '').lower()
+            next_gene = all_lines[i + 1][0:5].strip().replace('>', '').lower()
+
+            if (curr_gene != 'gene' and next_gene != 'gene'):
+                all_lines[i] = all_lines[i].strip() + all_lines.pop(i + 1).strip()
+
+    if all_lines[0][0:5].strip().replace('>', '').lower() == 'nari':
+
+        for i in range(len(all_lines) - 2, -1, -1):
+            curr_gene = all_lines[i][0:5].strip().replace('>', '').lower()
+            next_gene = all_lines[i + 1][0:5].strip().replace('>', '').lower()
+
+            if (curr_gene != 'nari' and next_gene != 'nari'):
+                all_lines[i] = all_lines[i].strip() + all_lines.pop(i + 1).strip()
+
+    # print(all_lines[::2])
     genes = [l.strip() for l in all_lines[::2]]
 
     fastas = [l.strip().upper() for l in all_lines[1::2]]
@@ -76,8 +92,11 @@ def convert_to_unique_genes(filename_in, filename_out):
     if len(gene_groups) > 1:
         raise NameError('Gene names differ between entries: {}'.format(s))
 
-    groups = [i for i, j in groupby(fastas)]
-    print(groups)
+    # groups = [i for i, j in groupby(fastas)]
+    groups = list(dict.fromkeys(fastas))
+    # print(mylist)
+    # print(groups)
+
 
     group_indices = []
     for g in groups:
@@ -88,10 +107,26 @@ def convert_to_unique_genes(filename_in, filename_out):
             # multi index group
             gene_comb_str = genes[group_indices[0]].split(']')[0].replace('tag', 'tags')
             # print(gene_comb_str)
+            # print(group_indices)
             for i in group_indices[1:]:
-                gene_comb_str += ',{}'.format(genes[group_indices[i]].split('=')[1].replace(']', ''))
+                # print(i)
+                # print(genes[i])
+                # gene_comb_str += ',{}'.format(genes[group_indices[i]].split('=')[1].replace(']', ''))
+                gene_comb_str += ',{}'.format(genes[i].split('=')[1].replace(']', ''))
             gene_comb_str += ']'
             return_str += '{}\n{}\n'.format(gene_comb_str, g)
+
+    genes2 = [l.strip() for l in return_str.split('\n')[::2] if l]
+    genes2_srt = sorted(genes2, key=lambda k: len(k), reverse=True)
+    genes3 = [genes2.index(g) for g in genes2_srt]
+
+    fastas2 = [l.strip() for l in return_str.split('\n')[1::2] if l]
+    fastas3 = [fastas2[i] for i in genes3]
+
+    res = [(i+'\n'+j) for i, j in zip(genes2_srt, fastas3)]
+
+    return_str = ''
+    return_str = '\n'.join(res)
 
     with open(filename_out, "w") as f:
         f.write(return_str)
@@ -108,20 +143,20 @@ def main():
         os.makedirs(directory)
 
     local = os.path.join(local, directory)
-    p = os.path.join(local, 'order_data.fasta')
+    p = os.path.join(local, 'narI.fasta')
 
     if not os.path.isfile(p):
-        urlretrieve(NARI_URL, p)
+        urlretrieve(url=NARI_URL, filename=p)
 
     # mimic fasta_dir function from test_unique_genes.py
-    simple_fasta = [
-        (">gene [locus_tag=AA11]", "AAAAAA"),
-        (">gene [locus_tag=BB22]", "AAAAAA"),
-        (">gene [locus_tag=CC33]", "AAAAAA"),
-        (">gene [locus_tag=DD44]", "GAAAAC"),
-    ]
+    # simple_fasta = [
+    #     (">gene [locus_tag=AA11]", "AAAAAA"),
+    #     (">gene [locus_tag=BB22]", "AAAAAA"),
+    #     (">gene [locus_tag=CC33]", "AAAAAA"),
+    #     (">gene [locus_tag=DD44]", "GAAAAC"),
+    # ]
 
-    print(simple_fasta)
+    # print(simple_fasta)
 
     # Regular 2-line FASTA file (1 line header, one line sequence)
     # filename = os.path.join(local, 'simple_test.fasta')
@@ -156,13 +191,23 @@ def main():
     # convert_to_unique_genes(filename, filename_o)
 
     # FASTA file with more than one gene
-    two_different_genes = simple_fasta.copy()
-    two_different_genes[0] = (">gene2 [locus_tag=AA11]", "AAAAAA")
-    print(two_different_genes)
-    filename = os.path.join(local, 'two_gene_names.fasta')
-    write_test_file(filename, two_different_genes)
-    filename_o = os.path.join(local, 'output4.fasta')
-    convert_to_unique_genes(filename, filename_o)
+    # two_different_genes = simple_fasta.copy()
+    # two_different_genes[0] = (">gene2 [locus_tag=AA11]", "AAAAAA")
+    # print(two_different_genes)
+    # filename = os.path.join(local, 'two_gene_names.fasta')
+    # write_test_file(filename, two_different_genes)
+    # filename_o = os.path.join(local, 'output4.fasta')
+    # convert_to_unique_genes(filename, filename_o)
+
+    # filename = os.path.join(local, 'narI.fasta')
+    # filename_o = os.path.join(local, 'output5.fasta')
+    # convert_to_unique_genes(filename, filename_o)
+    #
+    # with open(filename_o, "r") as f:
+    #     all_lines = f.readlines()
+    #
+    # print(sum([1 for line in all_lines if line[0] == ">"]))
+    # print(all_lines[0].strip().upper())
 
 
 if __name__ == "__main__":
