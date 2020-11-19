ba0abcbe472d320e85888edc89d85685b13ad039
nishant hegde <nhegde@netflix.com>
2020-11-18 16:04:54 -0800

    kata submission 1118

---
+++ 309/chaining_docs.py
@@ -0,0 +1,133 @@
+from __future__ import annotations
+
+import string
+
+EOL_PUNCTUATION = ".!?"
+
+
+class Document:
+    def __init__(self, lines=[]) -> None:
+        # it is up to you how to implement this method
+        # feel free to alter this method and its parameters to your liking
+        # raise NotImplementedError("You have to fill this method with life.")
+        self.lines = lines
+
+    def add_line(self, line: str, index: int = None) -> Document:
+        """Add a new line to the document.
+
+        Args:
+            line (str): The line,
+                expected to end with some kind of punctuation.
+            index (int, optional): The place where to add the line into the document.
+                If None, the line is added at the end. Defaults to None.
+
+        Returns:
+            Document: The changed document with the new line.
+        """
+        # raise NotImplementedError("You have to fill this method with life.")
+        if index:
+            self.lines.insert(index, line)
+        else:
+            self.lines.append(line)
+
+        return self
+
+    def swap_lines(self, index_one: int, index_two: int) -> Document:
+        """Swap two lines.
+
+        Args:
+            index_one (int): The first line.
+            index_two (int): The second line.
+
+        Returns:
+            Document: The changed document with the swapped lines.
+        """
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    def merge_lines(self, indices: list) -> Document:
+        """Merge several lines into a single line.
+
+        If indices are not in a row, the merged line is added at the first index.
+
+        Args:
+            indices (list): The lines to be merged.
+
+        Returns:
+            Document: The changed document with the merged lines.
+        """
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    def add_punctuation(self, punctuation: str, index: int) -> Document:
+        """Add punctuation to the end of a sentence.
+
+        Overwrites existing punctuation.
+
+        Args:
+            punctuation (str): The punctuation. One of EOL_PUNCTUATION.
+            index (int): The line to change.
+
+        Returns:
+            Document: The document with the changed line.
+        """
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    def word_count(self) -> int:
+        """Return the total number of words in the document."""
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    @property
+    def words(self) -> list:
+        """Return a list of unique words, sorted and case insensitive."""
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    def _remove_puctuation(line: str) -> str:
+        """Remove punctuation from a line."""
+        # you can use this function as helper method for
+        # Document.word_count() and Document.words
+        # or you can totally ignore it
+        pass
+
+    def __len__(self):
+        """Return the length of the document (i.e. line count)."""
+        # raise NotImplementedError("You have to fill this method with life.")
+        pass
+
+    def __str__(self):
+        """Return the content of the document as string."""
+        # raise NotImplementedError("You have to fill this method with life.")
+        ret = ''
+        for l in self.lines:
+            ret += l + '\n'
+
+        return ret.strip()
+
+
+def main():
+    print('thank you for looking after my mama and the waves this morning...')
+
+
+if __name__ == "__main__":
+    # this part is only execute when you run the file and is ignored by the tests
+    # you can use this section for debugging and testing
+    # d = (
+    #     Document()
+    #     .add_line("My first sentence.")
+    #     .add_line("My second sentence.")
+    #     .add_line("Introduction", 0)
+    #     .merge_lines([1, 2])
+    # )
+    d = Document()
+    d.add_line("first")
+    d.add_line("fourth")
+    d.add_line("third")
+    d.add_line("second")
+    print(d)
+    # print(len(d))
+    # print(d.word_count())
+    # print(d.words)
+    main()
