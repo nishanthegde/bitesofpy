49610e4a7f1690b3d3a3c9175f5f862f0d67e122
nishant hegde <nhegde@netflix.com>
2020-12-28 11:55:12 -0800

    kata submission 1228

---
+++ 318/decode.py
@@ -0,0 +1,30 @@
+import base64
+import csv
+from typing import List
+from io import StringIO
+
+csv1 = b"""
+Zmlyc3RfbmFtZSxsYXN0X25hbWUsY3JlZGl0X2NhcmQKS2VlbGJ5LE1hY0NhZmZlcmt5LD
+YzOTM3MTk0MzMzMjk5MjQKTGlubmVsbCxDbGVtbWV0dCwzNTU1NTg0OTI0MDkzOTU0CkVs
+eXNoYSxNZWlnaGFuLDYzODU3OTU3OTM4OTcxMDYKS2F0YWxpbixFdGhlcnRvbiwzNTg0Mj
+MwMDExNjgwNzAwCkZpbmEsUGFzZWssNTEwMDEzNjYzMTY2NDY4NwpBcmRlbGxhLEJyYXpp
+ZXIsMjAxNzEyNjEzNjUzMzc0CkRvcnRoZWEsS2FycGluc2tpLDMwNTAyNjYxMjUxMTcyCl
+Jhbm5hLER1ZmYsMzU3NjM5MzA1NjQ5MzMxMgpDaW5uYW1vbixLYWFzbWFuLDU0NDIwMjgx
+NTA4MDg1NzAKSmFjbGluLFRvbmdlLDM1NDk4NTIxMDQ3MjQ1MjcK
+"""
+
+
+def get_credit_cards(data: bytes) -> List[str]:
+    """Decode the base64 encoded data which gives you a csv
+    of "first_name,last_name,credit_card", from which you have
+    to extract the credit card numbers.
+    """
+
+    data_bytes = base64.b64decode(data)
+    data_string = data_bytes.decode("ascii")
+
+    f = StringIO(data_string)
+
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    return [row[2] for row in reader]
