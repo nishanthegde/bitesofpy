c69af5c9e32ab038f5a2a7e15f10500d00caf0be
nishant hegde <nhegde@netflix.com>
2020-12-30 11:19:23 -0800

    kata submission 1230a

--- 312/objects.py
+++ 312/objects.py
@@ -1,6 +1,8 @@
 from typing import Dict, List
 import keyword
 import sys as s
+import builtins
+import pkgutil
 
 scores = {
     "builtin": 1,
@@ -12,16 +14,28 @@ scores = {
 def score_objects(objects: List[str],
                   scores: Dict[str, int] = scores) -> int:
     score = 0
+    modules = []
+
+    for _, module, package in list(pkgutil.iter_modules()):
+        modules.append(module)
+
+    modules += list(s.builtin_module_names)
 
     for object in objects:
 
         if keyword.iskeyword(object):
+            print('kw')
             score += scores["keyword"]
 
-        if object in dir(__builtins__):
+        if object in dir(builtins):
+            print('bi')
             score += scores["builtin"]
 
-        if object in list(s.modules.keys()):
+        if object in modules:
+            print('mod')
             score += scores["module"]
 
     return score
+
+
+
