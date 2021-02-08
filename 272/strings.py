e87b1681c6698ac42522b588ec114ef57971741b
nishant hegde <nishant.arun.hegde@gmail.com>
2021-02-08 06:22:24 -0800

    missed doing kata

--- 272/strings.py
+++ 272/strings.py
@@ -1,16 +1,5 @@
 from typing import List
 
-sentence1 = ['To', 'be', 'or', 'not', 'to', 'be',
-             'that', 'is', 'a', 'question']
-sentence2 = ['To', 'strive', 'to', 'seek', 'to',
-             'find', 'and', 'not', 'to', 'yield']
-sentence3 = ['No', 'two', 'persons', 'ever', 'to',
-             'read', 'the', 'same', 'book', 'You', 'said']
-sentence4 = ['The', 'more', 'read', 'the',
-             'more', 'things', 'will', 'know']
-sentence5 = ['be', 'a', 'good', 'man']
-
-
 def common_words(sentence1: List[str], sentence2: List[str]) -> List[str]:
     """
     Input:  Two sentences - each is a  list of words in case insensitive ways.
