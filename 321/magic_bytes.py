660dbae546416b8bcad5985dc56907abb7de177d
nishant hegde <nishant.arun.hegde@gmail.com>
2021-02-16 11:26:45 -0800

    kata 0216

--- 321/magic_bytes.py
+++ 321/magic_bytes.py
@@ -24,12 +24,21 @@ tiff","Tagged Image File Format (TIFF)"
 "50 31 0A","P1.",0,"pbm","Portable bitmap"
 """  # noqa: E501
 
+CUSTOM_MAGIC_TABLE2 = """
+"magic_bytes","text_representation","offset","extension","description"
+"01 70 79 62 69 74 65 73 00 01 22 ?? ?? ?? 66 6F 72 6D 61 74 (little endian)
+01 70 79 62 69 74 65 73 01 00 22 ?? ?? ?? 66 6F 72 6D 61 74 (big endian)",".pybites..""...format
+.pybites..""...format","1","pybites
+pbf2","Fantasy pybites file format"
+"""  # noqa: E501
+
 
 class FileNotRecognizedException(Exception):
     """
     File cannot be identified using a magic table
     """
     pass
+
 
 def determine_filetype_by_magic_bytes(
         file_name: Union[str, pathlib.Path],
@@ -46,6 +55,7 @@ def determine_filetype_by_magic_bytes(
 
     bytes = open(file_name, "rb").read(32)
     hex_bytes = " ".join(['{:02X}'.format(byte) for byte in bytes])
+    # print(hex_bytes)
 
     magic = StringIO(lookup_table_string)
     reader = csv.reader(magic, delimiter=',')
@@ -55,11 +65,13 @@ def determine_filetype_by_magic_bytes(
 
     for row in reader:
         parsed_row = [element.split('\n') for element in row]
-        parsed_row.insert(0, [re.sub("\s*[\(].*?[\)\]]", "", byte_seq) for byte_seq in parsed_row[0]])
+        parsed_row.insert(0, [re.sub(r"\s*[\(].*?[\)\]]", "", byte_seq) for byte_seq in parsed_row[0]])
         parsed_row.pop(1)
         # print(parsed_row)
         for byte_seq in parsed_row[0]:
-            if byte_seq in hex_bytes:
+            regex = re.compile(byte_seq.replace('?', '.'))
+            # if byte_seq in hex_bytes:
+            if re.search(regex, hex_bytes):
                 no_matches = False
                 return parsed_row[4][0]
 
@@ -100,29 +112,14 @@ if __name__ == "__main__":
     # print("Testing file format")
     # print(determine_filetype_by_magic_bytes(test_filename))
 
-    test_filename = "test_image.tif"
+    test_filename = "test_image.pybites"
     print(f"Script invoked directly. Writing out test file {test_filename}")
     with open(test_filename, "wb") as f:
         f.write(
-                b"II*\x00\x0c\x00\x00\x00\xff\xff\xff\x00\x10\x00\x00\x01\x03"
-                b"\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x01\x03\x00\x01"
-                b"\x00\x00x00\x01\x00\x00\x00\x02\x01\x03\x00\x03\x00\x00\x00"
-                b"\xe2\x00\x00\x00\x03\x01\x03\x00\x01\x00\x00\x00\x01\x00"
-                b"\x00\x00\x06\x01\x03\x00\x01\x00\x00\x00\x02\x00\x00\x00"
-                b"\x11\x01\x04\x00\x01\x00\x00\x00\x08\x00\x00\x00\x12\x01"
-                b"\x03\x00\x01\x00\x00\x00\x01\x00\x00\x00\x15\x01\x03\x00"
-                b"\x01\x00\x00\x00\x03\x00\x00\x00\x16\x01\x03\x00\x01\x00"
-                b"\x00\x00\x80\x00\x00\x00\x17\x01\x04\x00\x01\x00\x00\x00"
-                b"\x03\x00\x00\x00\x1a\x01\x05\x00\x01\x00\x00\x00\xd2\x00"
-                b"\x00\x00\x1b\x01\x05\x00\x01\x00\x00\x00\xda\x00\x00\x00"
-                b"\x1c\x01\x03\x00\x01\x00\x00\x00\x01\x00\x00\x00\x1d\x01"
-                b"\x02\x00\x0b\x00\x00\x00\xee\x00\x00\x00(\x01\x03\x00\x01"
-                b"\x00\x00\x00\x02\x00\x00\x00S\x01\x03\x00\x03\x00\x00\x00"
-                b"\xe8\x00\x00\x00\x00\x00\x00\x00,\x01\x00\x00\x01\x00\x00"
-                b"\x00,\x01\x00\x00\x01\x00\x00\x00\x08\x00\x08\x00\x08\x00"
-                b"\x01\x00\x01\x00\x01\x00Background\x00"
+            b'\xee\x01pybites\x00\x01"\x00\xec\xedformat\x01\x00\x00\x00'
+            b"\x00\x01"
         )
     print("Testing file format")
-    print(determine_filetype_by_magic_bytes(test_filename))
+    print(determine_filetype_by_magic_bytes(test_filename, CUSTOM_MAGIC_TABLE2))
 
     main()
