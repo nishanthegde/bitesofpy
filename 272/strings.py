4464fe948c1e13f7eb278348bdab6e75087f0e2b
nishant hegde <nishant.arun.hegde@gmail.com>
2021-08-31 14:08:01 -0700

    kata submission 831

--- 272/strings.py
+++ 272/strings.py
@@ -1,26 +1,23 @@
 from typing import List
 
+sentence1 = ['To', 'be', 'or', 'not', 'to', 'be',
+             'that', 'is', 'a', 'question']
+sentence2 = ['To', 'strive', 'to', 'seek', 'to',
+             'find', 'and', 'not', 'to', 'yield']
+
+
 def common_words(sentence1: List[str], sentence2: List[str]) -> List[str]:
     """
     Input:  Two sentences - each is a  list of words in case insensitive ways.
-    Output: those common words appearing in both sentences. Capital and lowercase
-            words are treated as the same word.
+    Output: those common words appearing in both sentences. Capital and lowercase 
+            words are treated as the same word. 
 
-            If there are duplicate words in the results, just choose one word.
+            If there are duplicate words in the results, just choose one word. 
             Returned words should be sorted by word's length.
     """
-    ret = set([w.lower() for w in sentence1]) & set([w.lower() for w in sentence2])
-    return sorted(ret, key=lambda k: sentence1.index(k), reverse=True)
+    s1 = set([word.lower() for word in sentence1])
+    s2 = set([word.lower() for word in sentence2])
 
+    both = list(s1.intersection(s2))
 
-# def main():
-#     print('thank you for looking after my mama!')
-#     S = ['You', 'can', 'do', 'anything', 'but', 'not', 'everything']
-#     T = ['We', 'are', 'what', 'we', 'repeatedly', 'do', 'is', 'not', 'an', 'act']
-#
-#     print(common_words(S, T))
-#     print(common_words(sentence1, sentence2))
-#
-#
-# if __name__ == "__main__":
-#     main()
+    return sorted(both, key=len)
