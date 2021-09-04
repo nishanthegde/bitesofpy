143088ae17252d8be7bbe3c7d6adbd4a3c436b9e
nishant hegde <nishant.arun.hegde@gmail.com>
2021-09-04 13:31:41 -0700

    kata 0904

--- 329/script.py
+++ 329/script.py
@@ -36,60 +36,18 @@ def get_snake(data):
 
 
 def snake_case_keys(data):
-    # print(data)
     for key, value in data.items():
         if isinstance(value, dict):
-            # print(value)
             data[key] = snake_case_keys(value)
-            # level += 1
-            # keys.extend(get_keys(value, level))
-        # else:
+        elif isinstance(value, list):
+            data[key] = []
+            for item in value:
+                if isinstance(item, dict):
+                    data[key].append(snake_case_keys(item))
+                elif isinstance(item, list):
+                    data[key].append(
+                        [snake_case_keys(item_inner) if isinstance(item_inner, dict) else item_inner for item_inner in
+                         item])
+                else:
+                    data[key].append(item)
     return get_snake(data)
-
-    # data2 = data.copy()
-    #
-    # if not data or len(data) == 0:
-    #     return {}
-    #
-    # pattern = re.compile(r'(?=[A-Z-])')
-    # pattern2 = re.compile(r'(\D*)(\d+)')
-    #
-    # print(get_keys(data))
-    #
-    # for k in get_keys(data):
-    #     new_k = pattern.sub('_', k).lower().strip('_').replace('-', '')
-    #     if new_k != k:
-    #         new_k2 = ''
-    #         for m in re.finditer(pattern2, new_k):
-    #             new_k2 += m.group(1) + '_' + m.group(2)
-    #         if new_k2:
-    #             data2[new_k2] = data2[k]
-    #         else:
-    #             data2[new_k] = data2[k]
-    #         del data2[k]
-    #
-    # return data2
-
-
-def main():
-    print('thank you for looking after mama!')
-    # data = {"star": "wars"}
-    # data = {"DarthVader": "James Earl Jones"}
-    # data = {"executeOrder66": "yes sir!"}
-    data = {"firstName": "Han", "lastName": "Solo"}
-    # data = {
-    #     "darthVader": {
-    #         "firstName": "Anakin",
-    #         "lastName": "Skywalker",
-    #         "appearance": {
-    #             "helmetColor": "black",
-    #             "armorColor": "black",
-    #             "capeColor": "black",
-    #         },
-    #     }
-    # }
-    print(snake_case_keys(data))
-
-
-if __name__ == '__main__':
-    main()
