9e3c3eb48c00dae4578d1275916745f373c7db7b
nishant hegde <nishant.arun.hegde@gmail.com>
2021-09-15 12:59:59 -0700

    kata 0915

--- 326/astprinter.py
+++ 326/astprinter.py
@@ -57,20 +57,40 @@ class AstPrinter(ast.NodeVisitor):
                 d[attr_name] = attr_value
         return d
 
-    def visit(self, node):
+    def visit(self, node, ws_count=0):
         """trigger visit"""
 
         # define your logic to print the content of the tree
         #
         # you can use self._get_attrs() and self._get_children() to
         # separate the attributes in the two required types
-        ws_count = 0
-        print('{0}{1}'.format(self._get_name(node),'()'))
 
+        print('{0}{1}{2}'.format(ws_count * ' ', self._get_name(node), '()'))
+
+        # print(self._get_attrs(node))
         attr_dict = self._get_attrs(node)
-        for k, v in attr_dict.items():
-            ws_count += 3
-            print('{0}{1}{2}{3} {4}'.format(ws_count * ' ', '.', k, ':', v))
+        i = 0
+        for k in sorted(attr_dict):
+            if i == 0:
+                ws_count += 3
+            if isinstance(attr_dict[k], str):
+                print('{0}{1}{2}{3} \'{4}\''.format(ws_count * ' ', '.', k, ':', attr_dict[k]))
+            else:
+                print('{0}{1}{2}{3} {4}'.format(ws_count * ' ', '.', k, ':', attr_dict[k]))
+            i += 1
+
+        # print(self._get_children(node))
+        child_dict = self._get_children(node)
+        for k in child_dict:
+            print('{0}{1}{2}{3}'.format(ws_count * ' ', '.', k, ':'))
+            if self._is_list_of_nodes(child_dict[k]):
+                for n in child_dict[k]:
+                    ws_count += 3
+                    self.visit(n, ws_count)
+            elif self._is_node(child_dict[k]):
+                ws_count += 3
+                self.visit(child_dict[k], ws_count)
+            # print(k, v, self._is_node(k), self._get_name(v),self._is_list_of_nodes(v))
 
 
 def main():
@@ -116,3 +136,4 @@ one_plus_two = 1+2
     print(ast.dump(tree))
     vst = AstPrinter()
     vst.visit(tree)
+    print(CODE_ONE_LINE_AST)
