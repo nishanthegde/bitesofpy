ff0cd7733e0d4353e2b7cbb8ac78a64a730b0823
nishant hegde <nishant.arun.hegde@gmail.com>
2021-10-05 09:20:52 -0700

    kata 1005

--- 3/wordvalue.py
+++ 3/wordvalue.py
@@ -1,56 +1,47 @@
 import os
-import urllib.request as ur
+import urllib.request
 
-# PREWORK
-curr_path = os.getcwd()
-DICTIONARY = os.path.join('/tmp', 'dictionary.txt')
-ur.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+TMP = os.getenv("TMP", "/tmp")
 
-scrabble_scores = [(1, "E A O I N R T L S U")   , (2, "D G"), (3, "B C M P"),
+S3 = 'https://bites-data.s3.us-east-2.amazonaws.com/'
+DICT = 'dictionary.txt'
+DICTIONARY = os.path.join(TMP, DICT)
+urllib.request.urlretrieve(f'{S3}{DICT}', DICTIONARY)
+
+scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
                    (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
-
-LETTER_SCORES = {letter: score for score, letters in scrabble_scores for letter in letters.split()}
+LETTER_SCORES = {letter: score for score, letters in scrabble_scores
+                 for letter in letters.split()}
 
 
 def load_words():
-    """load the words dictionary (DICTIONARY constant) into a list and return it"""
-    words = []
+    """Load the words dictionary (DICTIONARY constant) into a list and return it"""
+    with open(DICTIONARY, "r") as f:
+        words = f.readlines()
 
-    f = open(DICTIONARY, 'r')
-    words = f.readlines()
-    f.close()
-
-    #take out newlines
-    words = [w[:-1] for w in words]
+    words = [w.strip() for w in words]
 
     return words
 
 
-def calc_word_value(word=None):
-    """given a word calculate its value using LETTER_SCORES"""
-    #list to hold letter score for the word
-    if word:
-        letter_score = [LETTER_SCORES[l.upper()] if l.upper() in LETTER_SCORES else 0 for l in word] #list to hold letter scores for word
+def calc_word_value(word):
+    """Given a word calculate its value using the LETTER_SCORES dict"""
+    score = 0
+    for letter in word:
+        if letter.upper() in LETTER_SCORES:
+            score += LETTER_SCORES[letter.upper()]
 
-        return sum(letter_score)
-
-    pass
-
-def max_word_value(words=None):
-    """given a list of words return the word with the maximum word value"""
-    #list to hold word score for the list of words
-    if words:
-        word_score = [calc_word_value(w) for w in words]
-
-        idx_max = word_score.index(max(word_score))
-
-        return words[idx_max]
-
-    pass
+    return score
 
 
+def max_word_value(words):
+    """Given a list of words calculate the word with the maximum value and return it"""
+    max_word = (None, 0)
+    for i, w in enumerate(words):
+        if calc_word_value(w) > max_word[1]:
+            max_word = (i, calc_word_value(w))
 
-
-
-
-
+    if max_word[0] == None:
+        return None
+    else:
+        return words[max_word[0]]
