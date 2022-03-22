d61a70fa72b7f53e9a2fb5d5fd7000416e3c80d0
nishant hegde <nishant.arun.hegde@gmail.com>
2022-03-22 14:19:55 -0700

    trimmed

--- 336/app.py
+++ 336/app.py
@@ -6,11 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to PyBites' FastAPI Learning Path 🐍 🎉"}
-
-
-def main():
-    print('please look after Mama and Naia')
-
-
-if __name__ == '__main__':
-    main()
