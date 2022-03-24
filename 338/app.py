d714d566dbb3d02bacc7c644fbca280af8b701f9
nishant hegde <nishant.arun.hegde@gmail.com>
2022-03-24 14:10:42 -0700

    kata 324

---
+++ 338/app.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from fastapi.testclient import TestClient
+from fastapi.responses import JSONResponse
+
+
+class Food(BaseModel):
+    """Model from Bite 02"""
+
+    id: int
+    name: str
+    serving_size: str
+    kcal_per_serving: int
+    protein_grams: float
+    fibre_grams: float = 0
+
+
+app = FastAPI()
+foods: Dict[int, Food] = {}
+
+
+@app.post("/", status_code=201)
+async def create_food(food: Food):
+    foods[food.id] = food
+    return food
