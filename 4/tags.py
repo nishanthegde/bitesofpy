2140613c7af71a3f675341d07a559810812589a4
nishant hegde <nishant.arun.hegde@gmail.com>
2022-04-25 09:26:07 -0700

    kata 425

--- 4/tags.py
+++ 4/tags.py
@@ -1,59 +1,29 @@
 import os
 from collections import Counter
+import re
 import urllib.request
 import xml.etree.ElementTree as ET
 
 # prep
-# local = os.getcwd()
-local = '/tmp'
-tempfile = os.path.join(local, 'feed')
-urllib.request.urlretrieve('http://bit.ly/2zD8d8b', tempfile)
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'feed')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/feed',
+    tempfile
+)
 
-tags = {}
-
-# open file
 with open(tempfile) as f:
     content = f.read().lower()
-# parse xml
 
-# get root element
-root = ET.fromstring(content)
-
-# get category
-for cat in root.iter('category'):
-
-    if cat.text not in tags:
-        tags[cat.text] = 1
-    else:
-        tags[cat.text] += 1
 
 def get_pybites_top_tags(n=10):
     """use Counter to get the top 10 PyBites tags from the feed
        data already loaded into the content variable"""
 
-    cntr = Counter(tags)
+    these_regex = "<category>(.+?)</category>"
+    pattern = re.compile(these_regex)
+    tags = re.findall(pattern, content)
 
-    return cntr.most_common(n)
+    c = Counter(tags)
 
-
-# def main():
-#     # open file
-#     with open(tempfile) as f:
-#         content = f.read().lower()
-#     # parse xml
-
-#     # get root element
-#     root = ET.fromstring(content)
-
-#     # get category
-#     for cat in root.iter('category'):
-
-#         if cat.text not in tags:
-#             tags[cat.text] = 1
-#         else:
-#             tags[cat.text] += 1
-
-#     print(get_pybites_top_tags())
-
-# if __name__ == "__main__":
-#     main()
+    return c.most_common()[:n]
