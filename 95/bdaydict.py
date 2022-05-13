a9a887fc29fdfabe62669055ecbbdbdf04984cb5
nishant hegde <nishant.arun.hegde@gmail.com>
2022-05-13 12:34:00 -0700

    kata 513

--- 95/bdaydict.py
+++ 95/bdaydict.py
@@ -7,34 +7,11 @@ class BirthdayDict(dict):
     """Override dict to print a message every time a new person is added that has
        the same birthday (day+month) as somebody already in the dict"""
 
-    def __init__(self, *args, **kwargs):
-        self.update(*args, **kwargs)
-
     def __setitem__(self, name, birthday):
-        # print('here ...')
 
-        for v in self.values():
-            if birthday.day == v.day and birthday.month == v.month:
+        for key in self:
+            if super(BirthdayDict, self).__getitem__(key).month == birthday.month \
+                    and super(BirthdayDict, self).__getitem__(key).day == birthday.day:
                 print(MSG.format(name))
 
-        super(BirthdayDict, self).__setitem__(str(name), birthday)
-
-
-# def main():
-
-#     # print('here ...')
-#     # print(MSG.format(2))
-
-#     bd = BirthdayDict()
-#     bd['bob'] = date(1987, 6, 15)
-#     bd['tim'] = date(1984, 7, 15)
-#     # print(bd)
-#     # bd.update({'sara': date(1978, 6, 14)})
-#     bd['mary'] = date(1987, 6, 15)
-#     # bd['sara'] = date(1987, 6, 14)
-#     bd['mike'] = date(1981, 7, 15)
-#     print(bd)
-
-
-# if __name__ == '__main__':
-#     main()
+        super(BirthdayDict, self).__setitem__(name, birthday)
