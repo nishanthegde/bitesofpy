65d090b11a4a112d9b94cc2cf43cbdc288b01b7b
nishant hegde <nishant.arun.hegde@gmail.com>
2022-06-29 16:11:37 -0700

    kata0629

--- 331/convolution.py
+++ 331/convolution.py
@@ -17,6 +17,14 @@ def convolution2D(
     Returns:
         np.array: the feature map constructed from the image and the kernel.
     """
+
+    if padding is None:
+        padding = (kernel.shape[0] - 1) / 2
+
+    if padding > 0:
+        image = np.pad(image, pad_width=int(padding), mode='constant', constant_values=0)
+
+    # return image
     return np.multiply(image, kernel)
 
 
@@ -24,9 +32,14 @@ def main():
     print("thank you for looking after mama and naia")
     KERNEL_1x1 = np.array([[1]])
     IMAGE_1x1 = np.array([[1]])
-    feature_map = convolution2D(IMAGE_1x1, KERNEL_1x1)
-    # print(feature_map.shape)
-    np.testing.assert_array_equal(feature_map.shape, (1, 1))
+    IMAGE_3x3 = np.random.rand(3, 3)
+    KERNEL_3x3_SHARPEN = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    # feature_map = convolution2D(IMAGE_1x1, KERNEL_1x1, 0, 1)
+    feature_map = convolution2D(IMAGE_3x3, KERNEL_3x3_SHARPEN, None, 1)
+    print(feature_map)
+    print(feature_map.shape)
+    # np.testing.assert_array_equal(feature_map.shape, (1, 1))
+
 
 if __name__ == "__main__":
     main()
