027f0d6e739cea6d746d19eef73c679d948ff3e1
nishant hegde <nishant.arun.hegde@gmail.com>
2022-09-24 18:46:53 -0700

    924a

--- 270/freq.py
+++ 270/freq.py
@@ -1,2 +1,8 @@
+from collections import Counter
+
+
 def freq_digit(num: int) -> int:
-    pass
+    num_str = str(num)
+    digit_count = Counter(num_str)
+
+    return int(digit_count.most_common(1)[0][0])
