eb2d413129afee687247165024542d37d51c37ae
nishant hegde <nishant.arun.hegde@gmail.com>
2022-09-25 17:59:07 -0700

    kata925

--- 180/names.py
+++ 180/names.py
@@ -13,40 +13,15 @@ Parrett,Ines,CN
 Braunle,Kermit,PL
 Halbard,Davie,CN"""
 
-data2 = """last_name,first_name,country_code
-Poxton,Sydney,CZ
-Kynman,Bryant,NL
-Mockler,Leese,AF
-Gillicuddy,Raffaello,IR
-Renyard,Carlo,CO
-Beadham,Evonne,CZ
-Tunstall,Allissa,IR
-Kamenar,Augy,IR
-Insko,Ave,NL
-Pigney,Gavrielle,ID"""
-
 
 def group_names_by_country(data: str = data) -> defaultdict:
     countries = defaultdict(list)
 
-    entries = data.split('\n')[1:]
-    for e in entries:
-        countries[e.split(',')[-1]].append('{} {}'.format(e.split(',')[1], e.split(',')[0]))
+    for line in data.splitlines():
+        line_list = line.split(',')
+
+        if line_list[0] != 'last_name':
+            full_name = line_list[1] + " " + line_list[0]
+            countries[line_list[2]].append(full_name)
 
     return countries
-
-
-# def main():
-#     print('here ...')
-#     grouping1 = group_names_by_country(data)
-#     assert type(grouping1) == defaultdict
-#     assert len(grouping1) == 7
-#     print(sorted(grouping1['CN']))
-
-#     grouping2 = group_names_by_country(data2)
-#     assert len(grouping2) == 6
-#     print(sorted(grouping2['IR']))
-
-
-# if __name__ == '__main__':
-#     main()
