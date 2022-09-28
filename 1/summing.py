63098b9d4f9997e5e572f6e81245225a77968039
nishant hegde <nishant.arun.hegde@gmail.com>
2022-09-27 18:57:38 -0700

    kata0927

--- 1/summing.py
+++ 1/summing.py
@@ -1,25 +1,8 @@
 def sum_numbers(numbers=None):
-
-    """
-        Function that accepts a list and returns the sum of the numbers in the list.
-        If no argumet is provided it calculates the sum of 1 to 100.
-    """
-
-    s = 0
-
-    if numbers != None:
-        s = sum(numbers)
+    if numbers == []:
+        return 0
     else:
-        for n in range(1,101):
-            s += n
+        if not numbers:
+            numbers = list(range(1, 101))
 
-    return s
-
-
-
-# r = sum_numbers(range(1, 11))
-# r = sum_numbers([])
-# r = sum_numbers()
-# r = sum_numbers(None)
-# r = sum_numbers(numbers=None)
-# print(r)
+    return sum(numbers)
