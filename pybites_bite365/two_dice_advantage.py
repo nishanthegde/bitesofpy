d46d5159a95aad897cd5d198f55161d8bd0fcb25
nishant hegde <nishant.arun.hegde@gmail.com>
2023-05-31 12:03:07 -0700

    kata submission 20230531

--- pybites_bite365/two_dice_advantage.py
+++ pybites_bite365/two_dice_advantage.py
@@ -12,12 +12,18 @@ def new_expected_value(n: int) -> float:
     """Calculate the expected value of an n-sided die when the player simultaneously rolls
     two dice and chooses the larger value.
     """
-    pass
+    outcomes = []
+    exp_value = 0
 
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            outcomes.append((i, j))
 
-def main():
-    print(original_expected_value(6))
+    for i in range(1, n + 1):
+        num_ways = 0
+        for outcome in outcomes:
+            if max(outcome) == i:
+                num_ways += 1
+        exp_value += i * num_ways / len(outcomes)
 
-
-if __name__ == "__main__":
-    main()
+    return round(exp_value, 3)
