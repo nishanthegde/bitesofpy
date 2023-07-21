c7dc6315835d78ece8c129a98a6d478752da964b
nishant hegde <nishant.arun.hegde@gmail.com>
2023-07-20 17:23:48 -0700

    kata 072023

--- pybites_bite46/fizzbuzz.py
+++ pybites_bite46/fizzbuzz.py
@@ -2,9 +2,24 @@ from typing import Union
 
 
 def fizzbuzz(num: int) -> Union[str, int]:
+    ret = []
+
     if num % 3 == 0:
-        return "Fizz"
-    elif num % 5 == 0:
-        return "Buzz"
-    else:
-        return num
+        ret.append("Fizz")
+
+    if num % 5 == 0:
+        ret.append("Buzz")
+
+    if num % 3 != 0 and num % 5 != 0:
+        ret.append(num)
+
+    return " ".join(map(str, ret))
+
+
+def main():
+    for i in range(1, 17):
+        print(fizzbuzz(i))
+
+
+if __name__ == "__main__":
+    main()
