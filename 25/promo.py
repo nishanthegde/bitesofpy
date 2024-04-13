89d9c90e950be396b3961f0c898a52a844b8ea30
nishant hegde <nishant.arun.hegde@gmail.com>
2024-04-12 20:24:52 -0700

    Add submission for bite 25

--- 25/promo.py
+++ 25/promo.py
@@ -15,103 +15,36 @@ BITES = {6: 'PyBites Die Hard',
          19: 'Write a simple property',
          20: 'Write a context manager',
          21: 'Query a nested data structure'}
-
-BITES_TEST = {6: 'PyBites Die Hard',
-              10: 'Practice exceptions',
-              16: 'Special PyBites date generator',
-              18: 'Find the most common word',
-              21: 'Query a nested data structure'}
-
-bites_done = {6, 10, 16, 18, 21}
-
-BITES_AVAILABLE = len(BITES) - len(bites_done)
+BITES_DONE = {6, 10, 16, 18, 21}
 
 
 class NoBitesAvailable(Exception):
-    pass
+    """There are no more Bites available to pick from"""
 
 
 class Promo:
 
-    def __init__(self, bites_done=bites_done):
-
-        self.bites_done = bites_done
+    def __init__(self):
+        # updated Bite to make local copies (avoid globals!)
+        self.all_bites = BITES.copy()
+        self.bites_done = BITES_DONE.copy()
 
     def _pick_random_bite(self):
+        """Pick a random Bite that is not done yet, if all
+           Bites are done, raise a NoBitesAvailable exception"""
+        remaining_bites = self.all_bites.keys() - self.bites_done
 
-        if not [k for k in list(BITES.keys()) if k not in self.bites_done]:
-            raise NoBitesAvailable("No more bites available")
-        else:
-            return random.choice([k for k in list(BITES.keys()) if k not in self.bites_done])
+        if not remaining_bites:
+            raise NoBitesAvailable("No remaining bites available")
+
+        random_bite = random.choice(list(remaining_bites))
+
+        return random_bite
 
     def new_bite(self):
-        nb = self._pick_random_bite()
-        self.bites_done.add(nb)
-        return nb
+        """Get  a random Bite using _pick_random_bite,
+           add it to self.bites_done, then return it"""
+        picked_bite_key = self._pick_random_bite()
+        self.bites_done.add(picked_bite_key)
 
-
-def grab_bites(promo, amount=BITES_AVAILABLE):
-    # _ is a throw-away variable (just want a loop)
-    for _ in range(amount):
-        promo.new_bite()
-
-
-def main():
-    print('here ...')
-    # print(type(BITES))
-
-    promo = Promo(bites_done=bites_done.copy())
-
-    assert len(BITES) == 15
-    assert len(promo.bites_done) == 5
-
-    for _ in range(10):
-        bite = promo._pick_random_bite()
-        # print(bite)
-        assert type(bite) == int
-        assert bite in BITES
-        assert bite not in promo.bites_done
-
-    # assert len(promo.bites_done) == 5
-    # grab_bites(promo, amount=7)
-    # # bites_done incremented with 7
-    # assert len(promo.bites_done) == 12
-    # # print(promo.bites_done)
-
-    assert len(promo.bites_done) == 5
-    grab_bites(promo)
-    # promo._pick_random_bite()
-    # exhausted bites
-    # with pytest.raises(NoBitesAvailable):
-    #     promo._pick_random_bite()
-
-    # grab_bites(promo)
-    # exhausted bites
-    # with pytest.raises(NoBitesAvailable):
-    #     promo._pick_random_bite()
-
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-    # p.new_bite()
-    # print(p.bites_done)
-
-
-if __name__ == '__main__':
-    main()
+        return picked_bite_key
